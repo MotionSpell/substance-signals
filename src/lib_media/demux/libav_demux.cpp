8c74f277ef92c295e5d11d88655f58e7ea2a89ae
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-10-05 09:47:13 +0200

    demux: increase fifo size to avoid errors

--- src/lib_media/demux/libav_demux.cpp
+++ src/lib_media/demux/libav_demux.cpp
@@ -73,7 +73,7 @@ LibavDemux::LibavDemux(const std::string &url) {
 			restampers[i] = uptr(create<Transform::Restamp>(Transform::Restamp::ClockSystem)); /*some webcams timestamps don't start at 0 (based on UTC)*/
 		}
 	} else {
-		ffpp::Dict dict(typeid(*this).name(), "demuxer", "-probesize 100M -analyzeduration 100M -overrun_nonfatal 1 -protocol_whitelist file,udp,rtp,http,https,tcp,tls");
+		ffpp::Dict dict(typeid(*this).name(), "demuxer", "-fifo_size 1000000 -probesize 100M -analyzeduration 100M -overrun_nonfatal 1 -protocol_whitelist file,udp,rtp,http,https,tcp,tls");
 		if (avformat_open_input(&m_formatCtx, url.c_str(), nullptr, &dict)) {
 			if (m_formatCtx) avformat_close_input(&m_formatCtx);
 			throw error(format("Error when opening input '%s'", url));
