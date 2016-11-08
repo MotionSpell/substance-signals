913978ed35d0237db593c5e7e5eece5610be801b
Rodolphe Fouquet <rodolphe.fouquet@gpac-licensing.com>
2016-11-08 18:53:16 -0500

    Adding a script to fix the libraries for macos

---
+++ fix-deps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+darwin_version=`uname -r`
+before_prefix="$PWD/extra/release/x86_64-apple-darwin$darwin_version/lib/"
+echo $before_prefix
+for i in $PWD/extra/lib/*dylib* 
+do
+  printf "Fixing deps for... $i\n"
+  libs=`otool -L $i | grep $before_prefix | cut -d ' ' -f 1`
+  for lib in $libs 
+  do
+    newlib=`echo $lib | sed -e "s|$before_prefix|$PWD/extra/lib/|g"`
+    install_name_tool -change $lib $newlib $i 
+  done 
+  has_rtmp=`otool -L $i | grep rtmp`
+  if [ -z "$has_rtmp" ]; then
+    install_name_tool -change librtmp.so.1 $PWD/extra/lib/librtmp.so.1 $i 
+  fi
+done
+install_name_tool -change librtmp.so.1 $PWD/extra/lib/librtmp.so.1 $PWD/extra/lib/librtmp.so.1
