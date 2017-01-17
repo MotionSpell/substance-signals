b9477e5f20ebe1cf21d6c754df14b0ab926bf005
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2017-01-17 18:10:22 +0100

    update gpac to fix linux crash on memory segments

--- extra/zen-extra.sh
+++ extra/zen-extra.sh
@@ -324,7 +324,7 @@ function gpac_build {
   host=$1
   pushDir $WORK/src
 
-  lazy_git_clone https://github.com/gpac/gpac.git gpac 5a40508dac
+  lazy_git_clone https://github.com/gpac/gpac.git gpac c4017e0a599
 
   local OS=$(get_os $host)
   local crossPrefix=$(get_cross_prefix $BUILD $host)
