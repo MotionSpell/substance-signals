5661fa35c8bd0f11bb4864c17fabc5fb098df980
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2017-05-09 15:22:48 +0200

    add missing files

---
+++ src/lib_media/in/mpeg_dash_input.cpp
@@ -0,0 +1,41 @@
+#include "lib_utils/tools.hpp"
+#include "mpeg_dash_input.hpp"
+
+#define IOSIZE (64*1024)
+
+namespace Modules {
+namespace In {
+
+MPEG_DASH_Input::MPEG_DASH_Input(std::string const &url) {
+	//GET MPD FROM HTTP
+
+	//PARSE MPD
+	
+	//DECLARE OUTPUT PINS
+
+}
+
+MPEG_DASH_Input::~MPEG_DASH_Input() {
+}
+
+void MPEG_DASH_Input::process(Data data) {
+	for (;;) {
+		if (getNumInputs() && getInput(0)->tryPop(data))
+			break;
+
+#if 0
+		auto out = output->getBuffer(IOSIZE);
+		size_t read = fread(out->data(), 1, IOSIZE, file);
+		if (read < IOSIZE) {
+			if (read == 0) {
+				break;
+			}
+			out->resize(read);
+		}
+		output->emit(out);
+#endif
+	}
+}
+
+}
+}
---
+++ src/lib_media/in/mpeg_dash_input.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "lib_modules/core/module.hpp"
+
+namespace Modules {
+
+namespace In {
+
+class MPEG_DASH_Input : public ModuleS {
+	public:
+		MPEG_DASH_Input(std::string const &url);
+		~MPEG_DASH_Input();
+		void process(Data data) override;
+
+	private:
+};
+
+}
+
+}
