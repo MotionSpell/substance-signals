f52a1e9e535982276e51efce4a29bcbb310c163f
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2017-07-12 12:38:28 +0200

    update zen extras to fix dash mpd@id conformance

--- extra/zen-extra.sh
+++ extra/zen-extra.sh
@@ -324,7 +324,7 @@ function gpac_build {
   host=$1
   pushDir $WORK/src
 
-  lazy_git_clone https://github.com/gpac/gpac.git gpac a8797d28ea
+  lazy_git_clone https://github.com/gpac/gpac.git gpac 51252746
 
   local OS=$(get_os $host)
   local crossPrefix=$(get_cross_prefix $BUILD $host)
