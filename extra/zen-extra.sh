b86c189e802e037f1bd9679aba33a121faf0d346
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2017-08-03 19:33:48 +0200

    revert wrong gpac extra revision

--- extra/zen-extra.sh
+++ extra/zen-extra.sh
@@ -324,7 +324,7 @@ function gpac_build {
   host=$1
   pushDir $WORK/src
 
-  lazy_git_clone https://github.com/gpac/gpac.git gpac f1141222
+  lazy_git_clone https://github.com/gpac/gpac.git gpac b74bbb4fb
 
   local OS=$(get_os $host)
   local crossPrefix=$(get_cross_prefix $BUILD $host)
