b019e6649fba9d68e7445924005d0502819c0367
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2018-01-19 09:54:10 +0100

    update cURL to 7.57.0

--- extra.sh
+++ extra.sh
@@ -145,7 +145,7 @@ if [ ! -f extra/src/curl/include/curl/curl.h ] ; then
 	rm -rf extra/src/curl
 	git clone https://github.com/curl/curl.git extra/src/curl
 	pushd extra/src/curl
-	git checkout curl-7_50_1
+	git checkout curl-7_57_0
 	autoreconf -fiv
 	popd
 fi
