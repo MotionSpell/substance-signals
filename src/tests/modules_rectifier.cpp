89d8d928c697090d85c282956e8a16f32a23b83c
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-03 17:21:27 +0200

    modules_rectifier.cpp: use namespace std, as it leads to less hairy prototypes

--- src/tests/modules_rectifier.cpp
+++ src/tests/modules_rectifier.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
 using namespace Tests;
 using namespace Modules;
 
@@ -29,10 +30,10 @@ class ClockMock : public IClock {
 			return 0.0;
 		}
 		void sleep(Fraction t) const override {
-			std::unique_lock<std::mutex> lock(mutex);
+			unique_lock<std::mutex> lock(mutex);
 			auto const tInit = time;
 			while (time < tInit + t) {
-				auto const durInMs = std::chrono::milliseconds(10);
+				auto const durInMs = chrono::milliseconds(10);
 				condition.wait_for(lock, durInMs);
 			}
 		}
@@ -40,12 +41,12 @@ class ClockMock : public IClock {
 	private:
 		Fraction time;
 		mutable std::mutex mutex;
-		mutable std::condition_variable condition;
+		mutable condition_variable condition;
 };
 
 unittest("scheduler: mock clock") {
 	std::mutex mutex;
-	std::condition_variable condition;
+	condition_variable condition;
 	Queue<Fraction> q;
 	auto f = [&](Fraction time) {
 		q.push(time);
@@ -61,8 +62,8 @@ unittest("scheduler: mock clock") {
 	ASSERT_EQUALS(0u, q.size());
 	clock->setTime(f10);
 	{
-		std::unique_lock<std::mutex> lock(mutex);
-		auto const durInMs = std::chrono::milliseconds(100);
+		unique_lock<std::mutex> lock(mutex);
+		auto const durInMs = chrono::milliseconds(100);
 		condition.wait_for(lock, durInMs);
 	}
 	ASSERT_EQUALS(1u, q.size());
@@ -78,7 +79,7 @@ struct DataGenerator : public ModuleS, public virtual IOutputCap {
 	}
 	void process(Data dataIn) override {
 		auto data = output->getBuffer(0);
-		auto dataPcm = std::dynamic_pointer_cast<DataPcm>(data);
+		auto dataPcm = dynamic_pointer_cast<DataPcm>(data);
 		if (dataPcm) {
 			dataPcm->setPlane(0, nullptr, 1024 * dataPcm->getFormat().getBytesPerSample());
 		}
@@ -89,18 +90,20 @@ struct DataGenerator : public ModuleS, public virtual IOutputCap {
 	PORT *output;
 };
 
-void testRectifierMeta(const Fraction &fps, std::shared_ptr<ClockMock> clock,
-    const std::vector<std::unique_ptr<ModuleS>> &generators,
-    const std::vector<std::vector<std::pair<int64_t, int64_t>>> &inTimes, const std::vector<std::vector<std::pair<int64_t, int64_t>>> &outTimes) {
+void testRectifierMeta(const Fraction &fps,
+    shared_ptr<ClockMock> clock,
+    const vector<unique_ptr<ModuleS>> &generators,
+    const vector<vector<pair<int64_t, int64_t>>> &inTimes,
+    const vector<vector<pair<int64_t, int64_t>>> &outTimes) {
 	auto rectifier = createModule<TimeRectifier>(1, clock, fps);
-	std::vector<std::unique_ptr<Utils::Recorder>> recorders;
+	vector<unique_ptr<Utils::Recorder>> recorders;
 	for (size_t g = 0; g < generators.size(); ++g) {
 		ConnectModules(generators[g].get(), 0, rectifier.get(), g);
 		recorders.push_back(create<Utils::Recorder>());
 		ConnectModules(rectifier.get(), g, recorders[g].get(), 0);
 	}
 
-	std::shared_ptr<DataRaw> data(new DataRaw(0));
+	shared_ptr<DataRaw> data(new DataRaw(0));
 	for (size_t g = 0; g < generators.size(); ++g) {
 		for (size_t i = 0; i < inTimes[g].size(); ++i) {
 			data->setMediaTime(inTimes[g][i].first);
@@ -117,7 +120,7 @@ void testRectifierMeta(const Fraction &fps, std::shared_ptr<ClockMock> clock,
 
 	for (size_t g = 0; g < generators.size(); ++g) {
 		recorders[g]->process(nullptr);
-		size_t i = 0, iMax = std::min<size_t>(inTimes[g].size(), outTimes[g].size());
+		size_t i = 0, iMax = min<size_t>(inTimes[g].size(), outTimes[g].size());
 		Data data;
 		while ((data = recorders[g]->pop()) && (i < iMax)) {
 			Log::msg(Debug, "recv[%s] %s-%s (expected %s-%s)", g, data->getMediaTime(), data->getClockTime(), outTimes[g][i].first, outTimes[g][i].second);
@@ -127,16 +130,16 @@ void testRectifierMeta(const Fraction &fps, std::shared_ptr<ClockMock> clock,
 		}
 		ASSERT(i >= iMax - 2);
 	}
-	clock->setTime(std::numeric_limits<int32_t>::max());
+	clock->setTime(numeric_limits<int32_t>::max());
 }
 
 template<typename Metadata, typename PortType>
-void testRectifierSinglePort(const Fraction &fps, const std::vector<std::pair<int64_t, int64_t>> &inTimes, const std::vector<std::pair<int64_t, int64_t>> &outTimes) {
-	std::vector<std::vector<std::pair<int64_t, int64_t>>> in;
+void testRectifierSinglePort(const Fraction &fps, const vector<pair<int64_t, int64_t>> &inTimes, const vector<pair<int64_t, int64_t>> &outTimes) {
+	vector<vector<pair<int64_t, int64_t>>> in;
 	in.push_back(inTimes);
-	std::vector<std::vector<std::pair<int64_t, int64_t>>> out;
+	vector<vector<pair<int64_t, int64_t>>> out;
 	out.push_back(outTimes);
-	std::vector<std::unique_ptr<ModuleS>> generators;
+	vector<unique_ptr<ModuleS>> generators;
 	auto clock = shptr(new ClockMock);
 	generators.push_back(createModule<DataGenerator<Metadata, PortType>>(in[0].size(), clock));
 	testRectifierMeta(fps, clock, generators, in, out);
@@ -144,12 +147,12 @@ void testRectifierSinglePort(const Fraction &fps, const std::vector<std::pair<in
 
 auto const generateValuesDefault = [](uint64_t step, Fraction fps) {
 	auto const t = timescaleToClock(step * fps.den, fps.num);
-	return std::pair<int64_t, int64_t>(t, t);
+	return pair<int64_t, int64_t>(t, t);
 };
 
-std::vector<std::pair<int64_t, int64_t>> generateData(Fraction fps, const std::function<std::pair<int64_t, int64_t>(uint64_t, Fraction)> &generateValue = generateValuesDefault) {
+vector<pair<int64_t, int64_t>> generateData(Fraction fps, const function<pair<int64_t, int64_t>(uint64_t, Fraction)> &generateValue = generateValuesDefault) {
 	auto const numItems = (size_t)(Fraction(15) * fps / Fraction(25, 1));
-	std::vector<std::pair<int64_t, int64_t>> times; times.resize(numItems);
+	vector<pair<int64_t, int64_t>> times; times.resize(numItems);
 	for (size_t i = 0; i < numItems; ++i) {
 		times[i] = generateValue(i, fps);
 	}
@@ -161,7 +164,7 @@ void testFPSFactor(const Fraction &fps, const Fraction &factor) {
 		auto const tIn = timescaleToClock(step * fps.den, fps.num);
 		auto const stepOutIn180k = (Clock::Rate * fps.den * factor.num) / (fps.num * factor.den);
 		auto const tOut = tIn / stepOutIn180k * stepOutIn180k;
-		return std::pair<int64_t, int64_t>(tIn, tOut);
+		return pair<int64_t, int64_t>(tIn, tOut);
 	};
 
 	auto const outTimes = generateData(fps * factor, genVal);
@@ -184,14 +187,14 @@ unittest("rectifier: initial offset (single port)") {
 	auto const fps = Fraction(25, 1);
 	auto const inGenVal = [&](uint64_t step, Fraction fps, int shift) {
 		auto const t = (int64_t)(Clock::Rate * (step + shift) * fps.den) / fps.num;
-		return std::pair<int64_t, int64_t >(t, (Clock::Rate * step * fps.den) / fps.num);
+		return pair<int64_t, int64_t >(t, (Clock::Rate * step * fps.den) / fps.num);
 	};
 
-	auto const outTimes = generateData(fps, std::bind(inGenVal, std::placeholders::_1, std::placeholders::_2, 0));
-	auto const inTimes1 = generateData(fps, std::bind(inGenVal, std::placeholders::_1, std::placeholders::_2, 5));
+	auto const outTimes = generateData(fps, bind(inGenVal, placeholders::_1, placeholders::_2, 0));
+	auto const inTimes1 = generateData(fps, bind(inGenVal, placeholders::_1, placeholders::_2, 5));
 	testRectifierSinglePort<MetadataRawVideo, OutputDataDefault<PictureYUV420P>>(fps, inTimes1, outTimes);
 
-	auto const inTimes2 = generateData(fps, std::bind(inGenVal, std::placeholders::_1, std::placeholders::_2, -5));
+	auto const inTimes2 = generateData(fps, bind(inGenVal, placeholders::_1, placeholders::_2, -5));
 	testRectifierSinglePort<MetadataRawVideo, OutputDataDefault<PictureYUV420P>>(fps, inTimes1, outTimes);
 }
 
@@ -203,7 +206,7 @@ unittest("rectifier: deal with missing frames (single port)") {
 		static uint64_t i = 0;
 		if (step && !(step % freq)) i++;
 		auto const t = (Clock::Rate * (step+i) * fps.den) / fps.num;
-		return std::pair<int64_t, int64_t >(t, t);
+		return pair<int64_t, int64_t >(t, t);
 	};
 	auto const inTimes = generateData(fps, inGenVal);
 
@@ -212,7 +215,7 @@ unittest("rectifier: deal with missing frames (single port)") {
 		static uint64_t prevT = 0, i = 1;
 		const uint64_t val = !(i % (freq+1)) ? prevT : t;
 		i++; prevT = t;
-		return std::pair<int64_t, int64_t >((Clock::Rate * step * fps.den) / fps.num, val);
+		return pair<int64_t, int64_t >((Clock::Rate * step * fps.den) / fps.num, val);
 	};
 	auto const outTimes = generateData(fps, outGenVal);
 
@@ -224,12 +227,12 @@ unittest("rectifier: deal with backward discontinuity (single port)") {
 	auto const outGenVal = [&](uint64_t step, Fraction fps, int64_t clockTimeOffset, int64_t mediaTimeOffset) {
 		auto const mediaTime = (int64_t)(Clock::Rate * (step + mediaTimeOffset) * fps.den) / fps.num;
 		auto const clockTime = (int64_t)(Clock::Rate * (step + clockTimeOffset) * fps.den) / fps.num;
-		return std::pair<int64_t, int64_t >(mediaTime, clockTime);
+		return pair<int64_t, int64_t >(mediaTime, clockTime);
 	};
 	auto inTimes1 = generateData(fps);
-	auto inTimes2 = generateData(fps, std::bind(outGenVal, std::placeholders::_1, std::placeholders::_2, inTimes1.size(), 0));
+	auto inTimes2 = generateData(fps, bind(outGenVal, placeholders::_1, placeholders::_2, inTimes1.size(), 0));
 	auto outTimes1 = generateData(fps);
-	auto outTimes2 = generateData(fps, std::bind(outGenVal, std::placeholders::_1, std::placeholders::_2, inTimes1.size(), inTimes1.size()));
+	auto outTimes2 = generateData(fps, bind(outGenVal, placeholders::_1, placeholders::_2, inTimes1.size(), inTimes1.size()));
 	inTimes1.insert(inTimes1.end(), inTimes2.begin(), inTimes2.end());
 	outTimes1.insert(outTimes1.end(), outTimes2.begin(), outTimes2.end());
 	testRectifierSinglePort<MetadataRawVideo, OutputDataDefault<PictureYUV420P>>(fps, inTimes1, outTimes1);
@@ -237,13 +240,13 @@ unittest("rectifier: deal with backward discontinuity (single port)") {
 
 unittest("rectifier: multiple media types simple") {
 	const Fraction fps1 = 25, fps2 = Fraction(44100, 1024);
-	std::vector<std::vector<std::pair<int64_t, int64_t>>> in;
+	vector<vector<pair<int64_t, int64_t>>> in;
 	in.push_back(generateData(fps1)); //simulate video
 	in.push_back(generateData(fps2)); //simulate audio
-	std::vector<std::vector<std::pair<int64_t, int64_t>>> out;
+	vector<vector<pair<int64_t, int64_t>>> out;
 	out.push_back(generateData(fps1));
 	out.push_back(generateData(fps2));
-	std::vector<std::unique_ptr<ModuleS>> generators;
+	vector<unique_ptr<ModuleS>> generators;
 	auto clock = shptr(new ClockMock);
 	generators.push_back(createModule<DataGenerator<MetadataRawVideo, OutputDataDefault<PictureYUV420P>>>(in[0].size(), clock));
 	generators.push_back(createModule<DataGenerator<MetadataRawAudio, OutputPcm>>(in[1].size(), clock));
