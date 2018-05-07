d1190bd2fb601ca31e45f9e36caadd4c65ebb262
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-07 14:49:00 +0200

    reformat.sh: don't use '-r' of astyle, as it forces us to also use '--preserve-date', which is currently broken (generates clock skews)

--- scripts/reformat.sh
+++ scripts/reformat.sh
@@ -6,12 +6,30 @@ if ! which astyle >/dev/null ; then
   exit 0
 fi
 
-astyle -q -r \
-  --lineend=linux \
-  --preserve-date \
-  --suffix=none \
-  --indent=tab \
-  --indent-after-parens \
-  --indent-classes --indent-col1-comments --style=attach --keep-one-line-statements \
-  '*.hpp' '*.cpp'
+function reformat_one_file
+{
+  local name="$1"
+
+  cat "$f" | astyle -q \
+    --lineend=linux \
+    --preserve-date \
+    --suffix=none \
+    --indent=tab \
+    --indent-after-parens \
+    --indent-classes \
+    --indent-col1-comments \
+    --style=attach \
+    --keep-one-line-statements > "$f.new.cpp"
+
+  if diff -u "$f" "$f.new.cpp" ; then
+    rm "$f.new.cpp"
+  else
+    mv "$f.new.cpp" "$f"
+  fi
+}
+
+find -name "*.hpp" -or -name "*.cpp" | while read f ; do
+  reformat_one_file "$f" &
+done
+
 
