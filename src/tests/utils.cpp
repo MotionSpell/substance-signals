789f8154d1a8807997464fc7484179a2a01f195a
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-09 17:33:55 +0200

    vcxproj: fix path of source files. The 'src/tests' directory is now almost agnostic to the various projects

--- src/tests/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include "tests.hpp"
-
-//#define ENABLE_FAILING_TESTS
-
-#include "utils_clock.cpp"
-#include "utils_fifo.cpp"
-#include "utils_misc.cpp"
-#include "utils_queue.cpp"
-#include "utils_scheduler.cpp"
