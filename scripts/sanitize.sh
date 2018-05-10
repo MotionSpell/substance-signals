1bcd9bbd1e9751b2a87c29158a86d221a656f3db
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-10 16:12:38 +0200

    sanitize.sh: fix comments

--- scripts/sanitize.sh
+++ scripts/sanitize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# Helper script to generate a coverage report for the full test suite.
-# Usage: ./cov.sh
-# The result is at: bin-cov/html/index.html
+# Helper script to run an instrumented version
+# of the unit tests.
+# Usage: ./scripts/sanitize.sh
 set -euo pipefail
 readonly scriptDir=$(dirname $(readlink -f $0))
 
@@ -11,6 +11,7 @@ export CFLAGS=-fsanitize=address,undefined
 export LDFLAGS=-fsanitize=address,undefined
 make -j`nproc`
 
+export TSAN_OPTIONS=halt_on_error=1
 export ASAN_OPTIONS=halt_on_error=1
 export UBSAN_OPTIONS=halt_on_error=1
 $scriptDir/run_tests.sh
