cb37c9666a54206c9d342ae9ea0dbc6b08f53544
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-23 09:52:47 +0200

    IClockCap: calling 'someModule->getClock' from the outside makes no sense, remove it from public interface

--- src/lib_modules/core/clock.hpp
+++ src/lib_modules/core/clock.hpp
@@ -6,7 +6,6 @@
 class IClockCap {
 	public:
 		virtual ~IClockCap() noexcept(false) {}
-		virtual const std::shared_ptr<IClock> getClock() const = 0; //FIXME: unused but otherwise class wouldn't be abstract
 
 	protected:
 		/*FIXME: we need to have factories to move these back to the implementation - otherwise ports created from the constructor may crash*/
@@ -17,10 +16,5 @@ class ClockCap : public virtual IClockCap {
 	public:
 		ClockCap(const std::shared_ptr<IClock> clock) {
 			this->clock = clock;
-		}
-
-	protected:
-		const std::shared_ptr<IClock> getClock() const override {
-			return clock;
 		}
 };
