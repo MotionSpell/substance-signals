e19c038c48c8bb2e3bfe632e14090f1b7a4b127a
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-24 10:49:14 +0200

    libav.hpp: remove unused declaration. The corresponding explicit template instanciation is already present in libav.cpp

--- src/lib_media/common/libav.hpp
+++ src/lib_media/common/libav.hpp
@@ -16,9 +16,6 @@ extern "C" {
 
 #define AV_PKT_FLAG_RESET_DECODER (1 << 30)
 
-template<>
-std::shared_ptr<AVCodecContext> shptr(AVCodecContext *p);
-
 namespace Modules {
 
 void AVCodecContextDeleter(AVCodecContext *p);
