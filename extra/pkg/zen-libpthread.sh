f20165729f6fdd1fea05e342c649f2997544ce9a
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-24 10:17:20 +0200

    zen-libpthread: upgrade mingw-w64: 3.1.0 -> 5.0.3
    This fixes a link issue with i686-w64-mingw32-gcc 7.3
    (undefined reference to __divmod4i)

--- extra/pkg/zen-libpthread.sh
+++ extra/pkg/zen-libpthread.sh
@@ -17,13 +17,11 @@ function libpthread_build {
 function libpthread_build_mingw {
   local host=$1
 
-  lazy_download "mingw-w64.tar.bz2" "http://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v3.1.0.tar.bz2/download"
+  lazy_download "mingw-w64.tar.bz2" "https://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v5.0.3.tar.bz2/download"
   lazy_extract "mingw-w64.tar.bz2"
   pushDir mingw-w64/mingw-w64-libraries
 
   autoconf_build $host "winpthreads"
-
-  rm $PREFIX/include/pthread.h #remove broken header - will fallback on the pthread correct one
 
   popDir
 }
