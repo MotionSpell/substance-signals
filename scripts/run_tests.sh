ec43df371ab79f93dd4da1df7a30e3bff0efd787
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-05-29 11:22:10 +0200

    scripts/run_tests.sh: blind-run the 3 apps so they appear in the coverage reports

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,7 +2,13 @@
 
 EXTRA=${EXTRA-$PWD/sysroot}
 
-PATH=$PATH:$EXTRA/bin:/mingw64/bin \
-LD_LIBRARY_PATH=$EXTRA/lib${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH:-} \
-DYLD_LIBRARY_PATH=$EXTRA/lib${DYLD_LIBRARY_PATH:+:}${DYLD_LIBRARY_PATH:-} \
+export PATH=$PATH:$EXTRA/bin:/mingw64/bin
+export LD_LIBRARY_PATH=$EXTRA/lib${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH:-}
+export DYLD_LIBRARY_PATH=$EXTRA/lib${DYLD_LIBRARY_PATH:+:}${DYLD_LIBRARY_PATH:-}
+
 make run
+
+# blind-run the apps so they appear in coverage reports
+$BIN/player.exe                       1>/dev/null 2>/dev/null || true
+$BIN/src/apps/dashcastx/dashcastx.exe 1>/dev/null 2>/dev/null || true
+$BIN/bin/src/apps/mp42tsx/mp42tsx.exe 1>/dev/null 2>/dev/null || true
