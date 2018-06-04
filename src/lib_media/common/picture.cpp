b3cb0dacf3e3e7d57c6fe2b751207db4d1aa2498
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-04 13:16:48 +0200

    'safe_cast' is for downcasts, don't use it for upcasts (which are already 'safe')

--- src/lib_media/common/picture.cpp
+++ src/lib_media/common/picture.cpp
@@ -6,15 +6,15 @@ std::shared_ptr<DataPicture> DataPicture::create(OutputPicture *out, const Resol
 	std::shared_ptr<DataPicture> r;
 	auto const size = PictureFormat::getSize(resInternal, format);
 	switch (format) {
-	case Y8:          r = safe_cast<DataPicture>(out->getBuffer<PictureY8>(size));          break;
-	case YUV420P:     r = safe_cast<DataPicture>(out->getBuffer<PictureYUV420P>(size));     break;
-	case YUV420P10LE: r = safe_cast<DataPicture>(out->getBuffer<PictureYUV420P10LE>(size)); break;
-	case YUV422P:     r = safe_cast<DataPicture>(out->getBuffer<PictureYUV422P>(size));     break;
-	case YUV422P10LE: r = safe_cast<DataPicture>(out->getBuffer<PictureYUV422P10LE>(size));     break;
-	case YUYV422:     r = safe_cast<DataPicture>(out->getBuffer<PictureYUYV422>(size));     break;
-	case NV12:        r = safe_cast<DataPicture>(out->getBuffer<PictureNV12   >(size));     break;
-	case RGB24:       r = safe_cast<DataPicture>(out->getBuffer<PictureRGB24  >(size));     break;
-	case RGBA32:      r = safe_cast<DataPicture>(out->getBuffer<PictureRGBA32 >(size));     break;
+	case Y8:          r = out->getBuffer<PictureY8>(size);          break;
+	case YUV420P:     r = out->getBuffer<PictureYUV420P>(size);     break;
+	case YUV420P10LE: r = out->getBuffer<PictureYUV420P10LE>(size); break;
+	case YUV422P:     r = out->getBuffer<PictureYUV422P>(size);     break;
+	case YUV422P10LE: r = out->getBuffer<PictureYUV422P10LE>(size); break;
+	case YUYV422:     r = out->getBuffer<PictureYUYV422>(size);     break;
+	case NV12:        r = out->getBuffer<PictureNV12   >(size);     break;
+	case RGB24:       r = out->getBuffer<PictureRGB24  >(size);     break;
+	case RGBA32:      r = out->getBuffer<PictureRGBA32 >(size);     break;
 	default: throw std::runtime_error("Unknown pixel format for DataPicture. Please contact your vendor");
 	}
 	r->setInternalResolution(resInternal);
