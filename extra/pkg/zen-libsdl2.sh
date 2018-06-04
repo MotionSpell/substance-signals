dc08cef75dee35a793a17e5601f6ecdc12647450
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-04 10:26:43 +0200

    Upgrade SDL2 + prevent the '-static-libgcc' flag from leaking SDL2's build system (incompatible with threads)

--- extra/pkg/zen-libsdl2.sh
+++ extra/pkg/zen-libsdl2.sh
@@ -2,11 +2,16 @@
 function libsdl2_build {
   local host=$1
 
-  lazy_download "libsdl2.tar.gz" "https://www.libsdl.org/release/SDL2-2.0.6.tar.gz"
+  lazy_download "libsdl2.tar.gz" "https://www.libsdl.org/release/SDL2-2.0.8.tar.gz"
   lazy_extract "libsdl2.tar.gz"
   mkgit "libsdl2"
 
   autoconf_build $host "libsdl2"
+
+  # fix SDL2 leaking flags to the user build system
+  # this one in particular is incompatible with the usage of <thread> and pthreads
+  # (see: gcc #52590)
+  sed -i "s/-static-libgcc//" $PREFIX/lib/pkgconfig/sdl2.pc
 }
 
 function libsdl2_get_deps {
