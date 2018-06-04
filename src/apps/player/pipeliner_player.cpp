adae30cbda662c993d83288d410ac52383fe3813
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-04 15:29:54 +0200

    Player: insert a GPACDemuxFull between the MPEG_Dash_In and the decoder

--- src/apps/player/pipeliner_player.cpp
+++ src/apps/player/pipeliner_player.cpp
@@ -2,6 +2,7 @@
 
 // modules
 #include "lib_media/demux/libav_demux.hpp"
+#include "lib_media/demux/gpac_demux_mp4_full.hpp"
 #include "lib_media/in/mpeg_dash_input.hpp"
 #include "lib_media/out/null.hpp"
 #include "lib_media/render/sdl_audio.hpp"
@@ -11,6 +12,7 @@
 using namespace Modules;
 using namespace Pipelines;
 using namespace In;
+using namespace Demux;
 
 static
 bool startsWith(std::string s, std::string prefix) {
@@ -37,24 +39,41 @@ void declarePipeline(Pipeline &pipeline, const char *url) {
 
 	std::unique_ptr<IFilePuller> createHttpSource();
 
-	auto createSource = [&](std::string url)->IPipelinedModule* {
-		if(startsWith(url, "http://")) {
-			return pipeline.addModule<MPEG_DASH_Input>(createHttpSource(), url);
-		} else
-			return pipeline.addModule<Demux::LibavDemux>(url);
+	struct OutputDesc {
+		IPipelinedModule* module;
+		int index;
 	};
 
-	auto demux = createSource(url);
+	auto createSources = [&](std::string url) -> std::vector<OutputDesc> {
+		std::vector<OutputDesc> r;
+		if(startsWith(url, "http://")) {
+			auto dashInput = pipeline.addModule<MPEG_DASH_Input>(createHttpSource(), url);
+			for (int i = 0; i < (int)dashInput->getNumOutputs(); ++i) {
+				auto demux = pipeline.addModule<GPACDemuxMP4Full>();
+				pipeline.connect(dashInput, i, demux, 0);
+				r.push_back({demux, 0});
+			}
+		} else {
+			auto demux = pipeline.addModule<Demux::LibavDemux>(url);
+			for (int i = 0; i < (int)demux->getNumOutputs(); ++i) {
+				r.push_back({demux, i});
+			}
+		}
+		return r;
+	};
 
-	for (int i = 0; i < (int)demux->getNumOutputs(); ++i) {
-		auto metadata = safe_cast<const MetadataPkt>(demux->getOutput(i)->getMetadata());
+	auto streams = createSources(url);
+
+	for (int k = 0; k < (int)streams.size(); ++k) {
+		auto s = streams[k];
+		auto metadata = safe_cast<const MetadataPkt>(s.module->getOutput(s.index)->getMetadata());
 		if (!metadata || metadata->isSubtitle()/*only render audio and video*/) {
-			Log::msg(Debug, "Ignoring stream #%s", i);
+			Log::msg(Debug, "Ignoring stream #%s", k);
 			continue;
 		}
 
 		auto decode = pipeline.addModule<Decode::Decoder>(metadata.get());
-		pipeline.connect(demux, i, decode, 0);
+		pipeline.connect(s.module, s.index, decode, 0);
 
 		auto render = createRenderer(metadata->getStreamType());
 		if (!render)
