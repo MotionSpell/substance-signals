b55711cb88296f9ba96a61a9e522f6375aedf9bc
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-04 09:38:57 +0200

    Upgrade libogg: fix MSYS2 build (failing at configure time because the 'build' platform (mingw64) was unsupported)

--- extra/pkg/zen-libogg.sh
+++ extra/pkg/zen-libogg.sh
@@ -2,7 +2,7 @@
 function libogg_build {
   host=$1
 
-  lazy_download "libogg.tar.gz" "http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz"
+  lazy_download "libogg.tar.gz" "http://downloads.xiph.org/releases/ogg/libogg-1.3.3.tar.gz"
   lazy_extract "libogg.tar.gz"
   mkgit "libogg"
 
