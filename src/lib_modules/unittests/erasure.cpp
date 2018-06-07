5f89bb2de01f712e3caa5f3c0e25a2d9db57b532
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-07 16:56:26 +0200

    Simplify: no need for specific Metadata type here anymore (again)

--- src/lib_modules/unittests/erasure.cpp
+++ src/lib_modules/unittests/erasure.cpp
@@ -17,7 +17,7 @@ secondclasstest("packet type erasure + multi-output: libav Demux -> {libav Decod
 	std::vector<std::unique_ptr<Decode::Decoder>> decoders;
 	std::vector<std::unique_ptr<Out::Print>> printers;
 	for (int i = 0; i < demux->getNumOutputs(); ++i) {
-		auto metadata = safe_cast<const MetadataPktLibav>(demux->getOutput(i)->getMetadata());
+		auto metadata = demux->getOutput(i)->getMetadata();
 		auto decode = create<Decode::Decoder>(metadata->getStreamType());
 
 		auto p = create<Out::Print>(std::cout);
