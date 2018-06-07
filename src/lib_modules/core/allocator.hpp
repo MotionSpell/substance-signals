6f7b062178c0a88db4f4e1657bc4e304c8d86228
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-07 16:10:03 +0200

    allocator.hpp: use a C++ constant rather than a #ifdef: this will ensure the conditioned code continues to compile.

--- src/lib_modules/core/allocator.hpp
+++ src/lib_modules/core/allocator.hpp
@@ -16,15 +16,13 @@ namespace Modules {
 static const size_t ALLOC_NUM_BLOCKS_DEFAULT = 10;
 static const size_t ALLOC_NUM_BLOCKS_LOW_LATENCY = 2;
 
-//#define ALLOC_NUM_BLOCKS_MAX_DYN_FREE /*free the dynamically allocated blocks*/
+auto const ALLOC_NUM_BLOCKS_MAX_DYN_FREE = 0 ;/*free the dynamically allocated blocks*/
 //#define ALLOC_DEBUG_TRACK_BLOCKS
 
 class PacketAllocator {
 	public:
 		PacketAllocator(size_t minBlocks, size_t maxBlocks) :
-#ifdef ALLOC_NUM_BLOCKS_MAX_DYN_FREE
 			minBlocks(minBlocks),
-#endif
 			maxBlocks(maxBlocks), curNumBlocks(minBlocks) {
 			if (minBlocks == 0)
 				throw std::runtime_error("Cannot create an allocator with 0 block.");
@@ -93,13 +91,13 @@ class PacketAllocator {
 		PacketAllocator& operator= (const PacketAllocator&) = delete;
 
 		void recycle(IData *p) {
-#ifdef ALLOC_NUM_BLOCKS_MAX_DYN_FREE
-			if (curNumBlocks > minBlocks) {
-				curNumBlocks--;
-				delete p;
-				return;
+			if(ALLOC_NUM_BLOCKS_MAX_DYN_FREE) {
+				if (curNumBlocks > minBlocks) {
+					curNumBlocks--;
+					delete p;
+					return;
+				}
 			}
-#endif
 			if (!p->isRecyclable()) {
 				delete p;
 				p = nullptr;
@@ -116,9 +114,7 @@ class PacketAllocator {
 			IData*data = nullptr;
 		};
 
-#ifdef ALLOC_NUM_BLOCKS_MAX_DYN_FREE
 		const size_t minBlocks;
-#endif
 		const size_t maxBlocks;
 		std::atomic_size_t curNumBlocks;
 		Queue<Block> freeBlocks;
