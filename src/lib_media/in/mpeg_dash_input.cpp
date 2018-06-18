9a4a65ddb1c874be4846f8f004176e4a5c94efd2
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-18 10:20:54 +0200

    Move HttpSource to mpeg_dash_demux.cpp

--- src/lib_media/in/mpeg_dash_input.cpp
+++ src/lib_media/in/mpeg_dash_input.cpp
@@ -238,66 +238,6 @@ DashMpd parseMpd(std::string text) {
 ///////////////////////////////////////////////////////////////////////////////
 // move this elsewhere
 
-extern "C" {
-#include <curl/curl.h>
-}
-
-struct HttpSource : IFilePuller {
-
-	HttpSource() : curl(curl_easy_init()) {
-		if(!curl)
-			throw std::runtime_error("can't init curl");
-	}
-
-	~HttpSource() {
-		curl_easy_cleanup(curl);
-	}
-
-	std::vector<uint8_t> get(std::string url) override {
-		struct HttpContext {
-			std::vector<uint8_t> data;
-
-			static size_t callback(void *stream, size_t size, size_t nmemb, void *ptr) {
-				auto pThis = (HttpContext*)ptr;
-				auto const bytes = size * nmemb;
-				pThis->onReceiveBuffer(bytes, (uint8_t*)stream);
-				return bytes;
-			}
-
-			void onReceiveBuffer(size_t size, uint8_t *stream) {
-				for(size_t i=0; i < size; ++i)
-					data.push_back(stream[i]);
-			}
-		};
-
-		HttpContext ctx;
-
-		// some servers require a user-agent field
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &HttpContext::callback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ctx);
-		curl_easy_setopt(curl, CURLOPT_FAILONERROR, true);
-
-		auto res = curl_easy_perform(curl);
-		if(res == CURLE_HTTP_RETURNED_ERROR)
-			return std::vector<uint8_t>();
-		if(res != CURLE_OK)
-			throw std::runtime_error(std::string("curl_easy_perform() failed: ") + curl_easy_strerror(res));
-
-		return ctx.data;
-	}
-
-	CURL* const curl;
-};
-
-std::unique_ptr<IFilePuller> createHttpSource() {
-	return make_unique<HttpSource>();
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// move this elsewhere
-
 // usage example:
 // map<string, string> vars;
 // vars["Name"] = "john";
