8ada0f92f0d12f347cd256b4e5dd0d9c717edf43
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-29 13:29:42 +0200

    Split muxer 'ugly' unit tests

--- src/lib_media/unittests/mux.cpp
+++ src/lib_media/unittests/mux.cpp
@@ -144,51 +144,65 @@ unittest("mux GPAC mp4: fragmented segments, one fragment per frame") {
 }
 
 // remove this when the below tests are split
-void operator+=(std::vector<Meta>& dst, std::vector<Meta> const& src) {
-	for(auto& val : src)
-		dst.push_back(val);
+std::vector<Meta> operator+(std::vector<Meta> const& a, std::vector<Meta> const& b) {
+	std::vector<Meta> r;
+	for(auto& val : a)
+		r.push_back(val);
+	for(auto& val : b)
+		r.push_back(val);
+	return r;
 }
 
 // causes valgrind errors and GPAC warnings
 secondclasstest("mux GPAC mp4 combination coverage: ugly") {
-	std::vector<Meta> results, ref = {
-		/* 03 */ { "", "audio/mp4", "mp4a.40.2", 0, 10437, 0, 1, 1 },
-		/* 05 */ { "output_video_gpac_12-0.mp4", "audio/mp4", "mp4a.40.2", 363629, 4838, 360000, 1, 1 },
-		/* 05 */ { "output_video_gpac_12-1.mp4", "audio/mp4", "mp4a.40.2", 359445, 4936, 359445, 1, 1 },
-		/* 05 */ { "output_video_gpac_12-2.mp4", "audio/mp4", "mp4a.40.2", 175543, 2838, 175543, 1, 1 },
-		/* 06 */ { "output_video_gpac_13-0.mp4", "audio/mp4", "mp4a.40.2", 363629, 19298, 360000, 1, 1 },
-		/* 06 */ { "output_video_gpac_13-1.mp4", "audio/mp4", "mp4a.40.2", 359445, 19232, 359445, 1, 1 },
-		/* 06 */ { "output_video_gpac_13-2.mp4", "audio/mp4", "mp4a.40.2", 175543, 9734, 175543, 1, 1 },
-		/* 07 */ { "output_video_gpac_14-0.mp4", "audio/mp4", "mp4a.40.2", 363629, 19298, 4180, 1, 1 },
-		/* 07 */ { "output_video_gpac_14-1.mp4", "audio/mp4", "mp4a.40.2", 359445, 19232, 4180, 1, 1 },
-		/* 07 */ { "output_video_gpac_14-2.mp4", "audio/mp4", "mp4a.40.2", 175543, 9734, 4180, 1, 1 },
-		/* 08 */ { "", "audio/mp4", "mp4a.40.2", 363629, 5226, 360000, 1, 1 },
-		/* 08 */ { "", "audio/mp4", "mp4a.40.2", 359445, 5336, 359445, 1, 1 },
-		/* 08 */ { "", "audio/mp4", "mp4a.40.2", 175543, 3022, 175543, 1, 1 },
-		/* 12 */ {  "", "audio/mp4", "mp4a.40.2", 0, 729, 0, 1, 1 },
-		/* 12 */ {  "", "audio/mp4", "mp4a.40.2", 363629, 4957, 360000, 1, 1 },
-		/* 12 */ {  "", "audio/mp4", "mp4a.40.2", 359445, 5047, 359445, 1, 1 },
-		/* 12 */ {  "", "audio/mp4", "mp4a.40.2", 175543, 2597, 175543, 1, 1 },
-		/* 13 */ {  "", "audio/mp4", "mp4a.40.2", 0, 729, 0, 1, 1 },
-		/* 13 */ {  "", "audio/mp4", "mp4a.40.2", 363629, 4949, 360000, 1, 0 },
-		/* 13 */ {  "", "audio/mp4", "mp4a.40.2", 0, 8, 0, 1, 1 },
-		/* 13 */ {  "", "audio/mp4", "mp4a.40.2", 359445, 5039, 359445, 1, 0 },
-		/* 13 */ {  "", "audio/mp4", "mp4a.40.2", 0, 8, 0, 1, 1 },
-		/* 13 */ {  "", "audio/mp4", "mp4a.40.2", 175543, 2589, 175543, 1, 0 },
-		/* 13 */ {  "", "audio/mp4", "mp4a.40.2", 0, 8, 0, 1, 1 },
+	std::vector<Meta> ref = {
+		{ "", "audio/mp4", "mp4a.40.2", 0, 10437, 0, 1, 1 },
+		{ "output_video_gpac_12-0.mp4", "audio/mp4", "mp4a.40.2", 363629, 4838, 360000, 1, 1 },
+		{ "output_video_gpac_12-1.mp4", "audio/mp4", "mp4a.40.2", 359445, 4936, 359445, 1, 1 },
+		{ "output_video_gpac_12-2.mp4", "audio/mp4", "mp4a.40.2", 175543, 2838, 175543, 1, 1 },
+		{ "output_video_gpac_13-0.mp4", "audio/mp4", "mp4a.40.2", 363629, 19298, 360000, 1, 1 },
+		{ "output_video_gpac_13-1.mp4", "audio/mp4", "mp4a.40.2", 359445, 19232, 359445, 1, 1 },
+		{ "output_video_gpac_13-2.mp4", "audio/mp4", "mp4a.40.2", 175543, 9734, 175543, 1, 1 },
+		{ "output_video_gpac_14-0.mp4", "audio/mp4", "mp4a.40.2", 363629, 19298, 4180, 1, 1 },
+		{ "output_video_gpac_14-1.mp4", "audio/mp4", "mp4a.40.2", 359445, 19232, 4180, 1, 1 },
+		{ "output_video_gpac_14-2.mp4", "audio/mp4", "mp4a.40.2", 175543, 9734, 4180, 1, 1 },
 	};
 
 	const uint64_t segmentDurationInMs = 2000;
 
-	results += runMux(create<Mux::GPACMuxMP4>("", 0, Mux::GPACMuxMP4::NoSegment, Mux::GPACMuxMP4::NoFragment)); // causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
-	results += runMux(create<Mux::GPACMuxMP4>("output_video_gpac_12", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::OneFragmentPerSegment, Mux::GPACMuxMP4::SegNumStartsAtZero)); // valgrind reports writes of uninitialized bytes
-	results += runMux(create<Mux::GPACMuxMP4>("output_video_gpac_13", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::OneFragmentPerRAP, Mux::GPACMuxMP4::SegNumStartsAtZero)); // valgrind reports writes of uninitialized bytes
-	results += runMux(create<Mux::GPACMuxMP4>("output_video_gpac_14", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::OneFragmentPerFrame, Mux::GPACMuxMP4::SegNumStartsAtZero)); // valgrind reports writes of uninitialized bytes
-	results += runMux(create<Mux::GPACMuxMP4>("", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::NoFragment, Mux::GPACMuxMP4::SegNumStartsAtZero)); // causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
-	results += runMux(create<Mux::GPACMuxMP4>("", segmentDurationInMs, Mux::GPACMuxMP4::FragmentedSegment, Mux::GPACMuxMP4::OneFragmentPerSegment, Mux::GPACMuxMP4::SegNumStartsAtZero));// causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
-	results += runMux(create<Mux::GPACMuxMP4>("", segmentDurationInMs, Mux::GPACMuxMP4::FragmentedSegment, Mux::GPACMuxMP4::OneFragmentPerSegment, Mux::GPACMuxMP4::SegNumStartsAtZero | Mux::GPACMuxMP4::FlushFragMemory));// causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
+	ASSERT_EQUALS(ref,
+	    runMux(create<Mux::GPACMuxMP4>("", 0, Mux::GPACMuxMP4::NoSegment, Mux::GPACMuxMP4::NoFragment)) // causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
+	    + runMux(create<Mux::GPACMuxMP4>("output_video_gpac_12", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::OneFragmentPerSegment, Mux::GPACMuxMP4::SegNumStartsAtZero)) // valgrind reports writes of uninitialized bytes
+	    + runMux(create<Mux::GPACMuxMP4>("output_video_gpac_13", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::OneFragmentPerRAP, Mux::GPACMuxMP4::SegNumStartsAtZero)) // valgrind reports writes of uninitialized bytes
+	    + runMux(create<Mux::GPACMuxMP4>("output_video_gpac_14", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::OneFragmentPerFrame, Mux::GPACMuxMP4::SegNumStartsAtZero)) // valgrind reports writes of uninitialized bytes
+	);
+}
 
-	ASSERT_EQUALS(ref, results);
+secondclasstest("mux GPAC mp4 combination coverage: ugly 2") {
+	std::vector<Meta> ref = {
+		{ "", "audio/mp4", "mp4a.40.2", 363629, 5226, 360000, 1, 1 },
+		{ "", "audio/mp4", "mp4a.40.2", 359445, 5336, 359445, 1, 1 },
+		{ "", "audio/mp4", "mp4a.40.2", 175543, 3022, 175543, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 0, 729, 0, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 363629, 4957, 360000, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 359445, 5047, 359445, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 175543, 2597, 175543, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 0, 729, 0, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 363629, 4949, 360000, 1, 0 },
+		{  "", "audio/mp4", "mp4a.40.2", 0, 8, 0, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 359445, 5039, 359445, 1, 0 },
+		{  "", "audio/mp4", "mp4a.40.2", 0, 8, 0, 1, 1 },
+		{  "", "audio/mp4", "mp4a.40.2", 175543, 2589, 175543, 1, 0 },
+		{  "", "audio/mp4", "mp4a.40.2", 0, 8, 0, 1, 1 },
+	};
+
+	const uint64_t segmentDurationInMs = 2000;
+
+	ASSERT_EQUALS(ref,
+	    runMux(create<Mux::GPACMuxMP4>("", segmentDurationInMs, Mux::GPACMuxMP4::IndependentSegment, Mux::GPACMuxMP4::NoFragment, Mux::GPACMuxMP4::SegNumStartsAtZero)) // causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
+	    + runMux(create<Mux::GPACMuxMP4>("", segmentDurationInMs, Mux::GPACMuxMP4::FragmentedSegment, Mux::GPACMuxMP4::OneFragmentPerSegment, Mux::GPACMuxMP4::SegNumStartsAtZero))// causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
+	    + runMux(create<Mux::GPACMuxMP4>("", segmentDurationInMs, Mux::GPACMuxMP4::FragmentedSegment, Mux::GPACMuxMP4::OneFragmentPerSegment, Mux::GPACMuxMP4::SegNumStartsAtZero | Mux::GPACMuxMP4::FlushFragMemory))// causes gpac warning: "[BS] Attempt to write on unassigned bitstream"
+	);
 }
 
 }
