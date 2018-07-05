77938413e2899b8ec7f41fe535f27e77631b8d25
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-07-05 09:14:18 +0200

    connection.hpp: avoid having a preprocessor symbol with the same name as a class member

--- src/lib_modules/core/connection.hpp
+++ src/lib_modules/core/connection.hpp
@@ -3,8 +3,6 @@
 namespace Modules {
 
 typedef Signals::IExecutor<void()> IProcessExecutor;
-extern Signals::ExecutorSync<void()> g_executorSync;
-#define defaultExecutor g_executorSync
 
 template<typename Class, typename Function>
 Signals::MemberFunctor<Class, void(Class::*)()>
@@ -12,7 +10,8 @@ Bind(Function func, Class* objectPtr) {
 	return Signals::MemberFunctor<Class, void(Class::*)()>(objectPtr, func);
 }
 
-void ConnectOutputToInput(IOutput *prev, IInput *next, IProcessExecutor * const executor = &defaultExecutor);
-void ConnectModules(IModule *prev, int outputIdx, IModule *next, int inputIdx, IProcessExecutor &executor = defaultExecutor);
+extern Signals::ExecutorSync<void()> g_executorSync;
+void ConnectOutputToInput(IOutput *prev, IInput *next, IProcessExecutor * const executor = &g_executorSync);
+void ConnectModules(IModule *prev, int outputIdx, IModule *next, int inputIdx, IProcessExecutor &executor = g_executorSync);
 
 }
