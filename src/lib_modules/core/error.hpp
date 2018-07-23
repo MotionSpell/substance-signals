2f5e2e09fe4b3cfc6d15e030f90e7efbe43075c3
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-07-23 17:22:20 +0200

    Remove unused class 'Exception'

--- src/lib_modules/core/error.hpp
+++ src/lib_modules/core/error.hpp
@@ -6,21 +6,10 @@
 
 namespace Modules {
 
-class Exception : public std::runtime_error {
-	public:
-		Exception(std::string const &msg) throw() : std::runtime_error(msg) {
-			Log::msg(Error, "Caught exception: %s", msg);
-		}
-		virtual ~Exception() throw() {}
-
-	private:
-		Exception& operator= (const Exception&) = delete;
-};
-
 class ErrorCap {
 	protected:
 		std::exception error(std::string const &msg) const {
-			throw Exception(format("[%s] %s", typeid(*this).name(), msg));
+			throw std::runtime_error(format("[%s] %s", typeid(*this).name(), msg));
 		}
 };
 
