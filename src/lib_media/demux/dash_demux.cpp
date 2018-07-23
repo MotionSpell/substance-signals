59fd302afe2819f58ef0e6e5e9d43eaef7c51e15
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-07-23 18:20:49 +0200

    Auto-reformat

--- src/lib_media/demux/dash_demux.cpp
+++ src/lib_media/demux/dash_demux.cpp
@@ -15,15 +15,15 @@ using namespace In;
 using namespace Transform;
 
 struct OutStub : ModuleS {
-	OutStub(OutputDefault *output) : output(output) {
-		addInput(new Input<DataBase>(this));
-	}
-	void process(Data data) override {
-		output->emit(data);
-	}
+		OutStub(OutputDefault *output) : output(output) {
+			addInput(new Input<DataBase>(this));
+		}
+		void process(Data data) override {
+			output->emit(data);
+		}
 
-private:
-	OutputDefault *output;
+	private:
+		OutputDefault *output;
 };
 
 DashDemuxer::DashDemuxer(std::string url) {
