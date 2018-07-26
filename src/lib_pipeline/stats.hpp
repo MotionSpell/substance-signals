3136ea0460690e96a5b88fb1e51b100f1246c0ab
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-07-26 12:51:13 +0200

    Fix entry size

--- src/lib_pipeline/stats.hpp
+++ src/lib_pipeline/stats.hpp
@@ -5,9 +5,12 @@
 namespace Pipelines {
 
 struct StatsEntry {
-	char name[255] = { 0 };
+	char name[252] {};
 	int32_t value = 0;
 };
+
+static_assert(sizeof(StatsEntry) == 256);
+
 struct IStatsRegistry {
 	virtual ~IStatsRegistry() {}
 	virtual StatsEntry* getNewEntry() = 0; /*owned by the StatsRegistry object*/
