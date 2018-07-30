3c9d3af752cdd73dadaa83e30557e68318e46bab
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-07-30 15:47:19 +0200

    Pipeline 'dump' tests: don't dynamically compute the 'expected', especially if it's non-deterministic (e.g ASLR)

--- src/lib_pipeline/unittests/graph.cpp
+++ src/lib_pipeline/unittests/graph.cpp
@@ -5,6 +5,7 @@
 
 using namespace Modules;
 using namespace Pipelines;
+using namespace std;
 
 namespace {
 
@@ -19,113 +20,129 @@ struct Dummy : public Module {
 	OutputDefault* output;
 };
 
+string replaceAll(string haystack, string needle, string with) {
+	auto pos = haystack.find(needle);
+
+	while( pos != std::string::npos) {
+		haystack.replace(pos, needle.size(), with);
+		pos = haystack.find(needle, pos + needle.size());
+	}
+
+	return haystack;
+}
+
+string formatPtr(void* p) {
+	stringstream ss;
+	ss << p;
+	return ss.str();
+}
+
 unittest("pipeline graph: empty") {
 	Pipeline p;
-	auto str = p.dump();
-	std::stringstream expected;
-	expected << "digraph {\n}\n";
-	ASSERT_EQUALS(expected.str(), str);
+	string expected = "digraph {\n}\n";
+	ASSERT_EQUALS(expected, p.dump());
 }
 
 unittest("pipeline graph: add module") {
 	Pipeline p;
-	auto ptr = p.addModule<Dummy>();
-	auto str = p.dump();
-	std::stringstream expected;
-	expected << "digraph {\n"
+	auto A = p.addModule<Dummy>();
+	string expected =
+	    "digraph {\n"
 	    "\tsubgraph cluster_0 {\n"
-	    "\t\tlabel = \"" << ptr << "\";\n"
+	    "\t\tlabel = \"A\";\n"
 	    "\t\tsubgraph cluster_inputs {\n"
 	    "\t\t\tlabel = \"inputs\";\n"
-	    "\t\t\t\"" << ptr << "_input_0\";\n"
+	    "\t\t\t\"A_input_0\";\n"
 	    "\t\t}\n"
 	    "\t\tsubgraph cluster_outputs {\n"
 	    "\t\t\tlabel = \"outputs\";\n"
-	    "\t\t\t\"" << ptr << "_output_0\";\n"
+	    "\t\t\t\"A_output_0\";\n"
 	    "\t\t}\n"
 	    "\t}\n\n"
 	    "}\n";
-	ASSERT_EQUALS(expected.str(), str);
+	auto str = replaceAll(p.dump(), formatPtr(A), "A");
+	ASSERT_EQUALS(expected, str);
 }
 
 unittest("pipeline graph: add connection") {
 	Pipeline p;
-	auto ptr1 = p.addModule<Dummy>();
-	auto ptr2 = p.addModule<Dummy>();
-	p.connect(ptr1, 0, ptr2, 0);
-	auto str = p.dump();
-	std::stringstream expected;
-	expected << "digraph {\n"
+	auto A = p.addModule<Dummy>();
+	auto B = p.addModule<Dummy>();
+	p.connect(A, 0, B, 0);
+	string expected =
+	    "digraph {\n"
 	    "\tsubgraph cluster_0 {\n"
-	    "\t\tlabel = \"" << ptr1 << "\";\n"
+	    "\t\tlabel = \"A\";\n"
 	    "\t\tsubgraph cluster_inputs {\n"
 	    "\t\t\tlabel = \"inputs\";\n"
-	    "\t\t\t\"" << ptr1 << "_input_0\";\n"
+	    "\t\t\t\"A_input_0\";\n"
 	    "\t\t}\n"
 	    "\t\tsubgraph cluster_outputs {\n"
 	    "\t\t\tlabel = \"outputs\";\n"
-	    "\t\t\t\"" << ptr1 << "_output_0\";\n"
+	    "\t\t\t\"A_output_0\";\n"
 	    "\t\t}\n"
 	    "\t}\n\n"
 	    "\tsubgraph cluster_1 {\n"
-	    "\t\tlabel = \"" << ptr2 << "\";\n"
+	    "\t\tlabel = \"B\";\n"
 	    "\t\tsubgraph cluster_inputs {\n"
 	    "\t\t\tlabel = \"inputs\";\n"
-	    "\t\t\t\"" << ptr2 << "_input_0\";\n"
+	    "\t\t\t\"B_input_0\";\n"
 	    "\t\t}\n"
 	    "\t\tsubgraph cluster_outputs {\n"
 	    "\t\t\tlabel = \"outputs\";\n"
-	    "\t\t\t\"" << ptr2 << "_output_0\";\n"
+	    "\t\t\t\"B_output_0\";\n"
 	    "\t\t}\n"
 	    "\t}\n\n"
-	    "\t\"" << ptr1 << "_output_0\" -> \"" << ptr2 << "_input_0\";\n"
+	    "\t\"A_output_0\" -> \"B_input_0\";\n"
 	    "}\n";
-	ASSERT_EQUALS(expected.str(), str);
+	auto str = p.dump();
+	str = replaceAll(str, formatPtr(A), "A");
+	str = replaceAll(str, formatPtr(B), "B");
+	ASSERT_EQUALS(expected, str);
 }
 
 unittest("pipeline graph: disconnect") {
 	Pipeline p;
-	auto ptr1 = p.addModule<Dummy>();
-	auto ptr2 = p.addModule<Dummy>();
-	p.connect(ptr1, 0, ptr2, 0);
-	p.disconnect(ptr1, 0, ptr2, 0);
-	auto str = p.dump();
-	std::stringstream expected;
-	expected << "digraph {\n"
+	auto A = p.addModule<Dummy>();
+	auto B = p.addModule<Dummy>();
+	p.connect(A, 0, B, 0);
+	p.disconnect(A, 0, B, 0);
+	string expected =
+	    "digraph {\n"
 	    "\tsubgraph cluster_0 {\n"
-	    "\t\tlabel = \"" << ptr1 << "\";\n"
+	    "\t\tlabel = \"A\";\n"
 	    "\t\tsubgraph cluster_inputs {\n"
 	    "\t\t\tlabel = \"inputs\";\n"
-	    "\t\t\t\"" << ptr1 << "_input_0\";\n"
+	    "\t\t\t\"A_input_0\";\n"
 	    "\t\t}\n"
 	    "\t\tsubgraph cluster_outputs {\n"
 	    "\t\t\tlabel = \"outputs\";\n"
-	    "\t\t\t\"" << ptr1 << "_output_0\";\n"
+	    "\t\t\t\"A_output_0\";\n"
 	    "\t\t}\n"
 	    "\t}\n\n"
 	    "\tsubgraph cluster_1 {\n"
-	    "\t\tlabel = \"" << ptr2 << "\";\n"
+	    "\t\tlabel = \"B\";\n"
 	    "\t\tsubgraph cluster_inputs {\n"
 	    "\t\t\tlabel = \"inputs\";\n"
-	    "\t\t\t\"" << ptr2 << "_input_0\";\n"
+	    "\t\t\t\"B_input_0\";\n"
 	    "\t\t}\n"
 	    "\t\tsubgraph cluster_outputs {\n"
 	    "\t\t\tlabel = \"outputs\";\n"
-	    "\t\t\t\"" << ptr2 << "_output_0\";\n"
+	    "\t\t\t\"B_output_0\";\n"
 	    "\t\t}\n"
 	    "\t}\n\n"
 	    "}\n";
-	ASSERT_EQUALS(expected.str(), str);
+	auto str = p.dump();
+	str = replaceAll(str, formatPtr(A), "A");
+	str = replaceAll(str, formatPtr(B), "B");
+	ASSERT_EQUALS(expected, str);
 }
 
 unittest("pipeline graph: remove module") {
 	Pipeline p;
 	auto ptr = p.addModule<Dummy>();
 	p.removeModule(ptr);
-	auto str = p.dump();
-	std::stringstream expected;
-	expected << "digraph {\n}\n";
-	ASSERT_EQUALS(expected.str(), str);
+	ASSERT_EQUALS("digraph {\n}\n", p.dump());
 }
 
 unittest("pipeline graph: remove wrong connection") {
