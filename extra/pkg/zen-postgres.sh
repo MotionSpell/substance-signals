c6751e8fb6c07459cd1a849416a501ce40d26cad
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2018-07-31 14:20:46 +0200

    fix extra (postgres) build on debia9

--- extra/pkg/zen-postgres.sh
+++ extra/pkg/zen-postgres.sh
@@ -2,11 +2,12 @@ function postgres_build {
   lazy_download "postgres.tar.bz2" "https://ftp.postgresql.org/pub/source/v10.4/postgresql-10.4.tar.bz2"
   lazy_extract "postgres.tar.bz2"
 
+  ac_cv_file__dev_urandom=yes \
   autoconf_build $host "postgres" \
-    "--with-openssl"
+    "--without-readline" \
+    "--with-system-tzdata=/usr/share/zoneinfo"
 }
 
 function postgres_get_deps {
   echo zlib
-  echo openssl
 }
