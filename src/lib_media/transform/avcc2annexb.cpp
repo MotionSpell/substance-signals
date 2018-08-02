1762f91e8c2d7487595cbfaa9566386d0af8f3eb
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-08-02 16:51:17 +0200

    Reduce scope of locals

--- src/lib_media/transform/avcc2annexb.cpp
+++ src/lib_media/transform/avcc2annexb.cpp
@@ -20,13 +20,12 @@ void AVCC2AnnexBConverter::process(Data in) {
 	av_packet_copy_props(out->getPacket(), safe_cast<const DataAVPacket>(in)->getPacket());
 
 	auto bs = gf_bs_new((const char*)in->data().ptr, in->data().len, GF_BITSTREAM_READ);
-	u64 availableBytes, pos;
-	while ( (availableBytes = gf_bs_available(bs)) ) {
+	while ( auto availableBytes = gf_bs_available(bs) ) {
 		if (availableBytes < 4) {
 			log(Error, "Need to read 4 byte start-code, only %s available. Exit current conversion.", availableBytes);
 			break;
 		}
-		pos = gf_bs_get_position(bs);
+		auto pos = gf_bs_get_position(bs);
 		auto const size = gf_bs_read_u32(bs);
 		if (size + 4 > availableBytes) {
 			log(Error, "Too much data read: %s (available: %s - 4) (total %s). Exit current conversion.", size, availableBytes, in->data().len);
