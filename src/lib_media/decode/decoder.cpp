51a02b9d84a79a058f210b3d1e57990cae9c639e
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-08-09 09:22:43 +0200

    Flatten 'Decoder' class + remove now-useless empty destructor

--- src/lib_media/decode/decoder.cpp
+++ src/lib_media/decode/decoder.cpp
@@ -16,20 +16,151 @@ using namespace Modules;
 
 namespace {
 
-class Decoder : public ModuleS, private PictureAllocator {
-	public:
-		Decoder(IModuleHost* host, StreamType type);
-		~Decoder();
-		void process(Data data) override;
-		void flush() override;
+struct Decoder : ModuleS, PictureAllocator {
+
+		Decoder(IModuleHost* host, StreamType type)
+			: m_host(host), avFrame(new ffpp::Frame) {
+			createInput(this);
+
+			if(type == VIDEO_PKT) {
+				videoOutput = addOutput<OutputPicture>(make_shared<MetadataRawVideo>());
+				getDecompressedData = std::bind(&Decoder::processVideo, this);
+			} else if(type == AUDIO_PKT) {
+				audioOutput = addOutput<OutputPcm>(make_shared<MetadataRawAudio>());
+				getDecompressedData = std::bind(&Decoder::processAudio, this);
+			} else
+				throw error("Can only decode audio or video.");
+		}
+
+		// IModule implementation
+		void process(Data data) {
+			inputs[0]->updateMetadata(data);
+
+			if(!codecCtx) {
+				auto meta = data->getMetadata();
+				if(!meta)
+					meta = inputs[0]->getMetadata();
+
+				if(!meta)
+					throw error("Can't instantiate decoder: no metadata for input data");
+
+				openDecoder(safe_cast<const MetadataPkt>(meta.get()));
+			}
+
+			assert(codecCtx);
+
+			if (data->flags & DATA_FLAGS_DISCONTINUITY) {
+				avcodec_flush_buffers(codecCtx.get());
+			}
+
+			AVPacket pkt {};
+			pkt.pts = data->getMediaTime();
+			pkt.data = (uint8_t*)data->data().ptr;
+			pkt.size = (int)data->data().len;
+			processPacket(&pkt);
+		}
+
+		void flush() {
+			if (codecCtx.get())
+				processPacket(nullptr);
+		}
 
 	private:
-		void openDecoder(const MetadataPkt* metadata);
-		void processPacket(AVPacket const * pkt);
-		std::shared_ptr<DataBase> processAudio();
-		std::shared_ptr<DataBase> processVideo();
-		void setMediaTime(DataBase* data);
-		PictureAllocator::PictureContext* getPicture(Resolution res, Resolution resInternal, PixelFormat format) override;
+		void openDecoder(const MetadataPkt* metadata) {
+			auto const extradata = metadata->codecSpecificInfo;
+
+			auto const codec = avcodec_find_decoder_by_name(metadata->codec.c_str());
+			if (!codec)
+				throw error(format("Decoder not found for codec '%s'.", metadata->codec));
+
+			codecCtx = shptr(avcodec_alloc_context3(codec));
+
+			// copy extradata: this allows decoding headerless bitstreams
+			// (i.e AVCC / H264-in-mp4).
+			{
+				codecCtx->extradata = (uint8_t*)av_calloc(1, extradata.size() + AV_INPUT_BUFFER_PADDING_SIZE);
+				codecCtx->extradata_size = (int)extradata.size();
+				if(extradata.data())
+					memcpy(codecCtx->extradata, extradata.data(), extradata.size());
+			}
+
+			ffpp::Dict dict(typeid(*this).name(), "-threads auto -err_detect 1 -flags output_corrupt -flags2 showall");
+			if (avcodec_open2(codecCtx.get(), codec, &dict) < 0)
+				throw error("Couldn't open stream.");
+
+			if (videoOutput) {
+				if (codecCtx->codec->capabilities & AV_CODEC_CAP_DR1) {
+					codecCtx->thread_safe_callbacks = 0;
+					codecCtx->opaque = static_cast<PictureAllocator*>(this);
+					codecCtx->get_buffer2 = avGetBuffer2;
+
+					// use a large number: some H.264 streams require up to 14 simultaneous buffers.
+					// this memory is lazily allocated anyway.
+					allocatorSize = 64;
+					videoOutput->resetAllocator(allocatorSize);
+				}
+			}
+		}
+
+		std::shared_ptr<DataBase> processAudio() {
+			auto out = audioOutput->getBuffer(0);
+			PcmFormat pcmFormat;
+			libavFrame2pcmConvert(avFrame->get(), &pcmFormat);
+			out->setFormat(pcmFormat);
+			for (uint8_t i = 0; i < pcmFormat.numPlanes; ++i) {
+				out->setPlane(i, avFrame->get()->data[i], avFrame->get()->nb_samples * pcmFormat.getBytesPerSample() / pcmFormat.numPlanes);
+			}
+
+			return out;
+		}
+
+		std::shared_ptr<DataBase> processVideo() {
+
+			std::shared_ptr<DataPicture> pic;
+			if (auto ctx = static_cast<PictureContext*>(avFrame->get()->opaque)) {
+				pic = ctx->pic;
+				ctx->pic->setVisibleResolution(Resolution(codecCtx->width, codecCtx->height));
+			} else {
+				pic = DataPicture::create(videoOutput, Resolution(avFrame->get()->width, avFrame->get()->height), libavPixFmt2PixelFormat((AVPixelFormat)avFrame->get()->format));
+				copyToPicture(avFrame->get(), pic.get());
+			}
+
+			return pic;
+		}
+
+		void setMediaTime(DataBase* data) {
+			data->setMediaTime(avFrame->get()->pts);
+		}
+
+		PictureAllocator::PictureContext* getPicture(Resolution res, Resolution resInternal, PixelFormat format) {
+			auto ctx = new PictureAllocator::PictureContext;
+			ctx->pic = DataPicture::create(videoOutput, res, resInternal, format);
+			return ctx;
+		}
+
+		void processPacket(AVPacket const * pkt) {
+			int ret;
+
+			ret = avcodec_send_packet(codecCtx.get(), pkt);
+			if (ret < 0) {
+				m_host->log(Warning, format("Decoding error: %s", avStrError(ret)).c_str());
+				return;
+			}
+
+			while(1) {
+				ret = avcodec_receive_frame(codecCtx.get(), avFrame->get());
+				if(ret != 0)
+					break; // no more frames
+
+				if (avFrame->get()->decode_error_flags || (avFrame->get()->flags & AV_FRAME_FLAG_CORRUPT)) {
+					m_host->log(Error, "Corrupted frame decoded");
+				}
+
+				auto data = getDecompressedData();
+				setMediaTime(data.get());
+				outputs[0]->emit(data);
+			}
+		}
 
 		IModuleHost* const m_host;
 		std::shared_ptr<AVCodecContext> codecCtx;
@@ -38,151 +169,6 @@ class Decoder : public ModuleS, private PictureAllocator {
 		OutputPcm *audioOutput = nullptr;
 		std::function<std::shared_ptr<DataBase>(void)> getDecompressedData;
 };
-
-Decoder::Decoder(IModuleHost* host, StreamType type)
-	: m_host(host), avFrame(new ffpp::Frame) {
-	createInput(this);
-
-	if(type == VIDEO_PKT) {
-		videoOutput = addOutput<OutputPicture>(make_shared<MetadataRawVideo>());
-		getDecompressedData = std::bind(&Decoder::processVideo, this);
-	} else if(type == AUDIO_PKT) {
-		audioOutput = addOutput<OutputPcm>(make_shared<MetadataRawAudio>());
-		getDecompressedData = std::bind(&Decoder::processAudio, this);
-	} else
-		throw error("Can only decode audio or video.");
-}
-
-void Decoder::openDecoder(const MetadataPkt* metadata) {
-	auto const extradata = metadata->codecSpecificInfo;
-
-	auto const codec = avcodec_find_decoder_by_name(metadata->codec.c_str());
-	if (!codec)
-		throw error(format("Decoder not found for codec '%s'.", metadata->codec));
-
-	codecCtx = shptr(avcodec_alloc_context3(codec));
-
-	// copy extradata: this allows decoding headerless bitstreams
-	// (i.e AVCC / H264-in-mp4).
-	{
-		codecCtx->extradata = (uint8_t*)av_calloc(1, extradata.size() + AV_INPUT_BUFFER_PADDING_SIZE);
-		codecCtx->extradata_size = (int)extradata.size();
-		if(extradata.data())
-			memcpy(codecCtx->extradata, extradata.data(), extradata.size());
-	}
-
-	ffpp::Dict dict(typeid(*this).name(), "-threads auto -err_detect 1 -flags output_corrupt -flags2 showall");
-	if (avcodec_open2(codecCtx.get(), codec, &dict) < 0)
-		throw error("Couldn't open stream.");
-
-	if (videoOutput) {
-		if (codecCtx->codec->capabilities & AV_CODEC_CAP_DR1) {
-			codecCtx->thread_safe_callbacks = 0;
-			codecCtx->opaque = static_cast<PictureAllocator*>(this);
-			codecCtx->get_buffer2 = avGetBuffer2;
-
-			// use a large number: some H.264 streams require up to 14 simultaneous buffers.
-			// this memory is lazily allocated anyway.
-			allocatorSize = 64;
-			videoOutput->resetAllocator(allocatorSize);
-		}
-	}
-}
-
-Decoder::~Decoder() {
-}
-
-std::shared_ptr<DataBase> Decoder::processAudio() {
-	auto out = audioOutput->getBuffer(0);
-	PcmFormat pcmFormat;
-	libavFrame2pcmConvert(avFrame->get(), &pcmFormat);
-	out->setFormat(pcmFormat);
-	for (uint8_t i = 0; i < pcmFormat.numPlanes; ++i) {
-		out->setPlane(i, avFrame->get()->data[i], avFrame->get()->nb_samples * pcmFormat.getBytesPerSample() / pcmFormat.numPlanes);
-	}
-
-	return out;
-}
-
-std::shared_ptr<DataBase> Decoder::processVideo() {
-
-	std::shared_ptr<DataPicture> pic;
-	if (auto ctx = static_cast<PictureContext*>(avFrame->get()->opaque)) {
-		pic = ctx->pic;
-		ctx->pic->setVisibleResolution(Resolution(codecCtx->width, codecCtx->height));
-	} else {
-		pic = DataPicture::create(videoOutput, Resolution(avFrame->get()->width, avFrame->get()->height), libavPixFmt2PixelFormat((AVPixelFormat)avFrame->get()->format));
-		copyToPicture(avFrame->get(), pic.get());
-	}
-
-	return pic;
-}
-
-void Decoder::setMediaTime(DataBase* data) {
-	data->setMediaTime(avFrame->get()->pts);
-}
-
-PictureAllocator::PictureContext* Decoder::getPicture(Resolution res, Resolution resInternal, PixelFormat format) {
-	auto ctx = new PictureAllocator::PictureContext;
-	ctx->pic = DataPicture::create(videoOutput, res, resInternal, format);
-	return ctx;
-}
-
-void Decoder::process(Data data) {
-	inputs[0]->updateMetadata(data);
-
-	if(!codecCtx) {
-		auto meta = data->getMetadata();
-		if(!meta)
-			meta = inputs[0]->getMetadata();
-
-		if(!meta)
-			throw error("Can't instantiate decoder: no metadata for input data");
-
-		openDecoder(safe_cast<const MetadataPkt>(meta.get()));
-	}
-
-	assert(codecCtx);
-
-	if (data->flags & DATA_FLAGS_DISCONTINUITY) {
-		avcodec_flush_buffers(codecCtx.get());
-	}
-
-	AVPacket pkt {};
-	pkt.pts = data->getMediaTime();
-	pkt.data = (uint8_t*)data->data().ptr;
-	pkt.size = (int)data->data().len;
-	processPacket(&pkt);
-}
-
-void Decoder::processPacket(AVPacket const * pkt) {
-	int ret;
-
-	ret = avcodec_send_packet(codecCtx.get(), pkt);
-	if (ret < 0) {
-		m_host->log(Warning, format("Decoding error: %s", avStrError(ret)).c_str());
-		return;
-	}
-
-	while(1) {
-		ret = avcodec_receive_frame(codecCtx.get(), avFrame->get());
-		if(ret != 0)
-			break; // no more frames
-
-		if (avFrame->get()->decode_error_flags || (avFrame->get()->flags & AV_FRAME_FLAG_CORRUPT)) {
-			m_host->log(Error, "Corrupted frame decoded");
-		}
-
-		auto data = getDecompressedData();
-		setMediaTime(data.get());
-		outputs[0]->emit(data);
-	}
-}
-
-void Decoder::flush() {
-	if (codecCtx.get())
-		processPacket(nullptr);
-}
 
 Modules::IModule* createObject(IModuleHost* host, va_list va) {
 	auto type = (StreamType)va_arg(va, int);
