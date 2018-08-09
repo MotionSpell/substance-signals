90e2b169a1a5f6f71fc179c92cbe454a48b5e69f
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-08-09 11:25:59 +0200

    dashcastx: remove config dead members

--- src/apps/dashcastx/config.hpp
+++ src/apps/dashcastx/config.hpp
@@ -13,15 +13,10 @@ struct Video { //FIXME: this can be factorized with other params
 struct Config {
 	std::string input;
 	std::string workingDir = ".";
-	std::string id;
 	std::vector<std::string> outputs;
 	std::vector<Video> v;
-	uint64_t seekTimeInMs = 0;
 	uint64_t segmentDurationInMs = 2000;
 	uint64_t timeshiftInSegNum = 0;
-	uint64_t minUpdatePeriodInMs = 0;
-	uint32_t minBufferTimeInMs = 0;
-	int64_t astOffset = 0;
 	bool isLive = false;
 	bool loop = false;
 	bool ultraLowLatency = false;
