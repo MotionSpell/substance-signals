f319b8b7cae69aad959b4434c2d7f2227bb80923
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-08-09 09:50:49 +0200

    Flatten 'AudioConvert' class + remove now-useless empty destructor

--- src/lib_media/transform/audio_convert.cpp
+++ src/lib_media/transform/audio_convert.cpp
@@ -44,19 +44,145 @@ struct Resampler {
 using namespace Modules;
 
 namespace {
-class AudioConvert : public ModuleS {
-	public:
+struct AudioConvert : ModuleS {
 		/*dstFrameSize is the number of output sample - '-1' is same as input*/
-		AudioConvert(IModuleHost* host, const PcmFormat &dstFormat, int64_t dstNumSamples = -1);
-		AudioConvert(IModuleHost* host, const PcmFormat &srcFormat, const PcmFormat &dstFormat, int64_t dstNumSamples = -1);
-		~AudioConvert();
-		void process(Data data) override;
-		void flush() override;
+		AudioConvert(IModuleHost* host, const PcmFormat &dstFormat, int64_t dstNumSamples)
+			: m_host(host),
+			  dstPcmFormat(dstFormat), dstNumSamples(dstNumSamples), m_Swr(nullptr), autoConfigure(true) {
+			srcPcmFormat = { 0 };
+			auto input = createInput(this);
+			input->setMetadata(make_shared<MetadataRawAudio>());
+			output = addOutput<OutputPcm>();
+		}
+
+		AudioConvert(IModuleHost* host, const PcmFormat &srcFormat, const PcmFormat &dstFormat, int64_t dstNumSamples)
+			:m_host(host),
+			 srcPcmFormat(srcFormat), dstPcmFormat(dstFormat), dstNumSamples(dstNumSamples), m_Swr(new Resampler), autoConfigure(false) {
+			configure(srcPcmFormat);
+			auto input = createInput(this);
+			input->setMetadata(make_shared<MetadataRawAudio>());
+			output = addOutput<OutputPcm>();
+		}
+
+		void process(Data data) {
+			uint64_t srcNumSamples = 0;
+			uint8_t * const * pSrc = nullptr;
+			auto audioData = safe_cast<const DataPcm>(data);
+			if (audioData) {
+				if (audioData->getFormat() != srcPcmFormat) {
+					if (autoConfigure) {
+						m_host->log(Info, "Incompatible input audio data. Reconfiguring.");
+						reconfigure(audioData->getFormat());
+						accumulatedTimeInDstSR = clockToTimescale(data->getMediaTime(), srcPcmFormat.sampleRate);
+					} else
+						throw error("Incompatible input audio data.");
+				}
+
+				srcNumSamples = audioData->size() / audioData->getFormat().getBytesPerSample();
+				if (dstNumSamples == -1) {
+					dstNumSamples = divUp(srcNumSamples * dstPcmFormat.sampleRate, (uint64_t)srcPcmFormat.sampleRate);
+				}
+				pSrc = audioData->getPlanes();
+			} else {
+				auto const delay = m_Swr->getDelay(dstPcmFormat.sampleRate);
+				if (delay == 0 && curDstNumSamples == 0) {
+					return;
+				} else if (delay < dstNumSamples) {
+					dstNumSamples = delay; //we are flushing, these are the last samples
+				}
+				pSrc = nullptr;
+				srcNumSamples = 0;
+			}
+
+			std::shared_ptr<DataPcm> out;
+			if (curDstNumSamples) {
+				out = curOut;
+			} else {
+				auto const dstBufferSize = dstNumSamples * dstPcmFormat.getBytesPerSample();
+				out = output->getBuffer(0);
+				out->setFormat(dstPcmFormat);
+				for (uint8_t i = 0; i < dstPcmFormat.numPlanes; ++i) {
+					out->setPlane(i, nullptr, (size_t)dstBufferSize / dstPcmFormat.numPlanes);
+				}
+			}
+
+			uint8_t* dstPlanes[AUDIO_PCM_PLANES_MAX];
+			for (int i=0; i<dstPcmFormat.numPlanes; ++i) {
+				dstPlanes[i] = out->getPlanes()[i] + curDstNumSamples * dstPcmFormat.getBytesPerSample() / dstPcmFormat.numPlanes;
+			}
+			int64_t targetNumSamples;
+			if (audioData) {
+				targetNumSamples = dstNumSamples - curDstNumSamples;
+			} else {
+				targetNumSamples = dstNumSamples;
+				dstNumSamples += curDstNumSamples;
+				const int64_t maxTargetNumSamples = out->getPlaneSize(0) * dstPcmFormat.numPlanes / dstPcmFormat.getBytesPerSample();
+				if (targetNumSamples + curDstNumSamples > maxTargetNumSamples) {
+					m_host->log(Warning, "Truncating last samples.");
+					targetNumSamples = maxTargetNumSamples;
+				}
+			}
+			assert(targetNumSamples >= 0);
+
+			auto const outNumSamples = m_Swr->convert(dstPlanes, (int)targetNumSamples, (const uint8_t**)pSrc, (int)srcNumSamples);
+
+			if (outNumSamples == targetNumSamples) {
+				curDstNumSamples = 0;
+				curOut = nullptr;
+				targetNumSamples = dstNumSamples;
+
+				auto const outPlaneSize = dstNumSamples * dstPcmFormat.getBytesPerSample() / dstPcmFormat.numPlanes;
+				for (uint8_t i = 0; i < dstPcmFormat.numPlanes; ++i)
+					out->setPlane(i, out->getPlane(i), (size_t)outPlaneSize);
+
+				auto const accumulatedTimeIn180k = timescaleToClock(accumulatedTimeInDstSR, dstPcmFormat.sampleRate);
+				out->setMediaTime(accumulatedTimeIn180k);
+				accumulatedTimeInDstSR += dstNumSamples;
+
+				output->emit(out);
+				out = nullptr;
+				if (m_Swr->getDelay(dstPcmFormat.sampleRate) >= dstNumSamples) { //accumulated more than one output buffer: flush.
+					process(nullptr);
+				}
+			} else if (outNumSamples < targetNumSamples) {
+				curDstNumSamples += outNumSamples;
+				curOut = out;
+			} else
+				throw error(format("Unexpected case: output %s samples when %s was requested (frame size = %s)", outNumSamples, targetNumSamples, dstNumSamples));
+		}
+
+		void flush() {
+			if (m_Swr)
+				process(nullptr);
+		}
+
+		void reconfigure(const PcmFormat &srcFormat) {
+			flush();
+			m_Swr = make_unique<Resampler>();
+			configure(srcFormat);
+			srcPcmFormat = srcFormat;
+		}
+
+		void configure(const PcmFormat &srcFormat) {
+			AVSampleFormat avSrcFmt, avDstFmt;
+			uint64_t avSrcChannelLayout, avDstChannelLayout;
+			int avSrcNumChannels, avDstNumChannels, avSrcSampleRate, avDstSampleRate;
+			libavAudioCtxConvertLibav(&srcFormat, avSrcSampleRate, avSrcFmt, avSrcNumChannels, avSrcChannelLayout);
+			libavAudioCtxConvertLibav(&dstPcmFormat, avDstSampleRate, avDstFmt, avDstNumChannels, avDstChannelLayout);
+
+			swr_alloc_set_opts(m_Swr->m_SwrContext,
+			    avDstChannelLayout,
+			    avDstFmt,
+			    avDstSampleRate,
+			    avSrcChannelLayout,
+			    avSrcFmt,
+			    avSrcSampleRate,
+			    0, nullptr);
+
+			m_Swr->init();
+		}
 
 	private:
-		void configure(const PcmFormat &srcFormat);
-		void reconfigure(const PcmFormat &srcFormat);
-
 		IModuleHost* const m_host;
 		PcmFormat srcPcmFormat;
 		PcmFormat const dstPcmFormat;
@@ -66,146 +192,8 @@ class AudioConvert : public ModuleS {
 		uint64_t accumulatedTimeInDstSR = 0;
 		OutputPcm *output;
 		bool autoConfigure;
+
 };
-
-AudioConvert::AudioConvert(IModuleHost* host, const PcmFormat &dstFormat, int64_t dstNumSamples)
-	: m_host(host),
-	  dstPcmFormat(dstFormat), dstNumSamples(dstNumSamples), m_Swr(nullptr), autoConfigure(true) {
-	srcPcmFormat = { 0 };
-	auto input = createInput(this);
-	input->setMetadata(make_shared<MetadataRawAudio>());
-	output = addOutput<OutputPcm>();
-}
-
-AudioConvert::AudioConvert(IModuleHost* host, const PcmFormat &srcFormat, const PcmFormat &dstFormat, int64_t dstNumSamples)
-	:m_host(host),
-	 srcPcmFormat(srcFormat), dstPcmFormat(dstFormat), dstNumSamples(dstNumSamples), m_Swr(new Resampler), autoConfigure(false) {
-	configure(srcPcmFormat);
-	auto input = createInput(this);
-	input->setMetadata(make_shared<MetadataRawAudio>());
-	output = addOutput<OutputPcm>();
-}
-
-AudioConvert::~AudioConvert() {
-}
-
-void AudioConvert::reconfigure(const PcmFormat &srcFormat) {
-	flush();
-	m_Swr = make_unique<Resampler>();
-	configure(srcFormat);
-	srcPcmFormat = srcFormat;
-}
-
-void AudioConvert::configure(const PcmFormat &srcFormat) {
-	AVSampleFormat avSrcFmt, avDstFmt;
-	uint64_t avSrcChannelLayout, avDstChannelLayout;
-	int avSrcNumChannels, avDstNumChannels, avSrcSampleRate, avDstSampleRate;
-	libavAudioCtxConvertLibav(&srcFormat, avSrcSampleRate, avSrcFmt, avSrcNumChannels, avSrcChannelLayout);
-	libavAudioCtxConvertLibav(&dstPcmFormat, avDstSampleRate, avDstFmt, avDstNumChannels, avDstChannelLayout);
-
-	swr_alloc_set_opts(m_Swr->m_SwrContext,
-	    avDstChannelLayout,
-	    avDstFmt,
-	    avDstSampleRate,
-	    avSrcChannelLayout,
-	    avSrcFmt,
-	    avSrcSampleRate,
-	    0, nullptr);
-
-	m_Swr->init();
-}
-
-void AudioConvert::flush() {
-	if (m_Swr)
-		process(nullptr);
-}
-
-void AudioConvert::process(Data data) {
-	uint64_t srcNumSamples = 0;
-	uint8_t * const * pSrc = nullptr;
-	auto audioData = safe_cast<const DataPcm>(data);
-	if (audioData) {
-		if (audioData->getFormat() != srcPcmFormat) {
-			if (autoConfigure) {
-				m_host->log(Info, "Incompatible input audio data. Reconfiguring.");
-				reconfigure(audioData->getFormat());
-				accumulatedTimeInDstSR = clockToTimescale(data->getMediaTime(), srcPcmFormat.sampleRate);
-			} else
-				throw error("Incompatible input audio data.");
-		}
-
-		srcNumSamples = audioData->size() / audioData->getFormat().getBytesPerSample();
-		if (dstNumSamples == -1) {
-			dstNumSamples = divUp(srcNumSamples * dstPcmFormat.sampleRate, (uint64_t)srcPcmFormat.sampleRate);
-		}
-		pSrc = audioData->getPlanes();
-	} else {
-		auto const delay = m_Swr->getDelay(dstPcmFormat.sampleRate);
-		if (delay == 0 && curDstNumSamples == 0) {
-			return;
-		} else if (delay < dstNumSamples) {
-			dstNumSamples = delay; //we are flushing, these are the last samples
-		}
-		pSrc = nullptr;
-		srcNumSamples = 0;
-	}
-
-	std::shared_ptr<DataPcm> out;
-	if (curDstNumSamples) {
-		out = curOut;
-	} else {
-		auto const dstBufferSize = dstNumSamples * dstPcmFormat.getBytesPerSample();
-		out = output->getBuffer(0);
-		out->setFormat(dstPcmFormat);
-		for (uint8_t i = 0; i < dstPcmFormat.numPlanes; ++i) {
-			out->setPlane(i, nullptr, (size_t)dstBufferSize / dstPcmFormat.numPlanes);
-		}
-	}
-
-	uint8_t* dstPlanes[AUDIO_PCM_PLANES_MAX];
-	for (int i=0; i<dstPcmFormat.numPlanes; ++i) {
-		dstPlanes[i] = out->getPlanes()[i] + curDstNumSamples * dstPcmFormat.getBytesPerSample() / dstPcmFormat.numPlanes;
-	}
-	int64_t targetNumSamples;
-	if (audioData) {
-		targetNumSamples = dstNumSamples - curDstNumSamples;
-	} else {
-		targetNumSamples = dstNumSamples;
-		dstNumSamples += curDstNumSamples;
-		const int64_t maxTargetNumSamples = out->getPlaneSize(0) * dstPcmFormat.numPlanes / dstPcmFormat.getBytesPerSample();
-		if (targetNumSamples + curDstNumSamples > maxTargetNumSamples) {
-			m_host->log(Warning, "Truncating last samples.");
-			targetNumSamples = maxTargetNumSamples;
-		}
-	}
-	assert(targetNumSamples >= 0);
-
-	auto const outNumSamples = m_Swr->convert(dstPlanes, (int)targetNumSamples, (const uint8_t**)pSrc, (int)srcNumSamples);
-
-	if (outNumSamples == targetNumSamples) {
-		curDstNumSamples = 0;
-		curOut = nullptr;
-		targetNumSamples = dstNumSamples;
-
-		auto const outPlaneSize = dstNumSamples * dstPcmFormat.getBytesPerSample() / dstPcmFormat.numPlanes;
-		for (uint8_t i = 0; i < dstPcmFormat.numPlanes; ++i)
-			out->setPlane(i, out->getPlane(i), (size_t)outPlaneSize);
-
-		auto const accumulatedTimeIn180k = timescaleToClock(accumulatedTimeInDstSR, dstPcmFormat.sampleRate);
-		out->setMediaTime(accumulatedTimeIn180k);
-		accumulatedTimeInDstSR += dstNumSamples;
-
-		output->emit(out);
-		out = nullptr;
-		if (m_Swr->getDelay(dstPcmFormat.sampleRate) >= dstNumSamples) { //accumulated more than one output buffer: flush.
-			process(nullptr);
-		}
-	} else if (outNumSamples < targetNumSamples) {
-		curDstNumSamples += outNumSamples;
-		curOut = out;
-	} else
-		throw error(format("Unexpected case: output %s samples when %s was requested (frame size = %s)", outNumSamples, targetNumSamples, dstNumSamples));
-}
 
 IModule* createObject(IModuleHost* host, va_list va) {
 	auto src = va_arg(va, PcmFormat*);
