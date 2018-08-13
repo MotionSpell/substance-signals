MotionSpell/substance-signals

commit 915e84b36b3285c21f8a7a95f9267cccfd475f59

DashDemuxer: hide Pipeline from header

src/lib_media/demux/dash_demux.cpp
@@ -4,6 +4,7 @@
 #include "lib_media/in/mpeg_dash_input.hpp"
 #include "lib_media/out/null.hpp"
 #include "lib_media/transform/restamp.hpp"
+#include "lib_pipeline/pipeline.hpp"
 
 std::unique_ptr<Modules::In::IFilePuller> createHttpSource();
 
@@ -14,7 +15,7 @@ using namespace In;
 using namespace Transform;
 
 DashDemuxer::DashDemuxer(std::string url) {
-	auto downloader = pipeline.addModule<MPEG_DASH_Input>(createHttpSource(), url);
+	auto downloader = pipeline->addModule<MPEG_DASH_Input>(createHttpSource(), url);
 
 	for (int i = 0; i < (int)downloader->getNumOutputs(); ++i)
 		addStream(downloader->getOutput(i));
@@ -28,19 +29,24 @@ void DashDemuxer::addStream(IOutput* downloadOutput) {
 	output->setMetadata(meta);
 
 	// add MP4 demuxer
-	auto decap = pipeline.addModule<GPACDemuxMP4Full>();
+	auto decap = pipeline->addModule<GPACDemuxMP4Full>();
 	ConnectOutputToInput(downloadOutput, decap->getInput(0));
 
 	// add restamper (so the timestamps start at zero)
-	auto restamp = pipeline.addModule<Restamp>(Transform::Restamp::Reset);
+	auto restamp = pipeline->addModule<Restamp>(Transform::Restamp::Reset);
 	ConnectOutputToInput(decap->getOutput(0), restamp->getInput(0));
 
 	ConnectOutput(restamp, [output](Data data) {
 		output->emit(data);
 	});
 
-	auto null = pipeline.addModule<Out::Null>();
-	pipeline.connect(restamp, 0, null, 0);
+	auto null = pipeline->addModule<Out::Null>();
+	pipeline->connect(restamp, 0, null, 0);
+}
+
+void DashDemuxer::process() {
+	pipeline->start();
+	pipeline->waitForEndOfStream();
 }
 
 }

src/lib_media/demux/dash_demux.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
-#include "lib_pipeline/pipeline.hpp"
+#include <lib_modules/utils/helper.hpp>
+
+namespace Pipelines {
+	class Pipeline;
+	struct IPipelinedModule;
+}
 
 namespace Modules {
 namespace Demux {
@@ -9,15 +14,12 @@ class DashDemuxer : public Module {
 	public:
 		DashDemuxer(std::string url);
 
-		virtual void process() override {
-			pipeline.start();
-			pipeline.waitForEndOfStream();
-		}
+		virtual void process() override;
 
 	private:
 		void addStream(IOutput* downloadOutput);
 
-		Pipelines::Pipeline pipeline;
+		std::unique_ptr<Pipelines::Pipeline> pipeline;
 };
 
 }

commit dcd04032a7448bd81fbe3d872ac38085cfc9e899

DashDemuxer: fix segfault
(introduced by 915e84b36b3285c21f8a7a95f9267cccfd475f59)

src/lib_media/demux/dash_demux.cpp
@@ -27,6 +27,7 @@ struct OutStub : ModuleS {
 };
 
 DashDemuxer::DashDemuxer(std::string url) {
+	pipeline = make_unique<Pipelines::Pipeline>();
 	auto downloader = pipeline->addModule<MPEG_DASH_Input>(createHttpSource(), url);
 
 	for (int i = 0; i < (int)downloader->getNumOutputs(); ++i)