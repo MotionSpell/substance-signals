c8de517bf4d88fa778f87a06adde17632cad6d5a
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-09-04 09:50:34 +0200

    Make ErrorCap::error consistent: either it always throws, or it returns an exception. But it can't do both

--- src/lib_modules/core/error.hpp
+++ src/lib_modules/core/error.hpp
@@ -8,7 +8,7 @@ namespace Modules {
 class ErrorCap {
 	protected:
 		std::exception error(std::string const &msg) const {
-			throw std::runtime_error("[" + std::string(typeid(*this).name()) + "]" + msg);
+			return std::runtime_error("[" + std::string(typeid(*this).name()) + "]" + msg);
 		}
 };
 
