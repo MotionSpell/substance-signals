04d74c08737e2af4795159bb8734b0a07979b894
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-09-04 10:20:46 +0200

    Extract method 'CheckMetadataCompatibility'

--- src/lib_modules/core/connection.cpp
+++ src/lib_modules/core/connection.cpp
@@ -7,7 +7,7 @@ namespace Modules {
 
 Signals::ExecutorSync g_executorSync;
 
-void ConnectOutputToInput(IOutput *prev, IInput *next, IExecutor * const executor) {
+void CheckMetadataCompatibility(IOutput *prev, IInput *next) {
 	auto prevMetadata = prev->getMetadata();
 	auto nextMetadata = next->getMetadata();
 	if (prevMetadata && nextMetadata) {
@@ -24,6 +24,10 @@ void ConnectOutputToInput(IOutput *prev, IInput *next, IExecutor * const executo
 			Log::msg(Debug, "--------- Connect: no metadata");
 		}
 	}
+}
+
+void ConnectOutputToInput(IOutput *prev, IInput *next, IExecutor * const executor) {
+	CheckMetadataCompatibility(prev, next);
 
 	next->connect();
 	prev->getSignal().connect([=](Data data) {
