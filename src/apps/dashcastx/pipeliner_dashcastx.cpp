a7fd2701d7c24214ea593a6def26581c0b710589
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-09-05 14:23:56 +0200

    Rename 'metadataDemux' to 'metadata': there's only one metadata we deal with here

--- src/apps/dashcastx/pipeliner_dashcastx.cpp
+++ src/apps/dashcastx/pipeliner_dashcastx.cpp
@@ -50,8 +50,8 @@ Resolution autoFit(Resolution input, Resolution output) {
 	}
 };
 
-IPipelinedModule* createEncoder(Pipeline* pipeline, Metadata metadataDemux, bool ultraLowLatency, VideoCodecType videoCodecType, PictureFormat &dstFmt, int bitrate, uint64_t segmentDurationInMs) {
-	auto const codecType = metadataDemux->type;
+IPipelinedModule* createEncoder(Pipeline* pipeline, Metadata metadata, bool ultraLowLatency, VideoCodecType videoCodecType, PictureFormat &dstFmt, int bitrate, uint64_t segmentDurationInMs) {
+	auto const codecType = metadata->type;
 	if (codecType == VIDEO_PKT) {
 		Log::msg(Info, "[Encoder] Found video stream");
 		EncoderConfig p { EncoderConfig::Video };
@@ -60,7 +60,7 @@ IPipelinedModule* createEncoder(Pipeline* pipeline, Metadata metadataDemux, bool
 		p.res = dstFmt.res;
 		p.bitrate = bitrate;
 
-		auto const metaVideo = safe_cast<const MetadataPktLibavVideo>(metadataDemux);
+		auto const metaVideo = safe_cast<const MetadataPktLibavVideo>(metadata);
 		p.frameRate = metaVideo->getFrameRate();
 		auto const GOPDurationDivisor = 1 + (segmentDurationInMs / (MAX_GOP_DURATION_IN_MS+1));
 		p.GOPSize = ultraLowLatency ? 1 : (Fraction(segmentDurationInMs, 1000) * p.frameRate) / Fraction(GOPDurationDivisor, 1);
@@ -76,7 +76,7 @@ IPipelinedModule* createEncoder(Pipeline* pipeline, Metadata metadataDemux, bool
 		return m;
 	} else if (codecType == AUDIO_PKT) {
 		Log::msg(Info, "[Encoder] Found audio stream");
-		auto const demuxFmt = toPcmFormat(safe_cast<const MetadataPktLibavAudio>(metadataDemux));
+		auto const demuxFmt = toPcmFormat(safe_cast<const MetadataPktLibavAudio>(metadata));
 		EncoderConfig p { EncoderConfig::Audio };
 		p.sampleRate = demuxFmt.sampleRate;
 		p.numChannels = demuxFmt.numChannels;
@@ -88,14 +88,14 @@ IPipelinedModule* createEncoder(Pipeline* pipeline, Metadata metadataDemux, bool
 }
 
 /*video is forced, audio is as passthru as possible*/
-IPipelinedModule* createConverter(Pipeline* pipeline, Metadata metadataDemux, Metadata metadataEncoder, const PictureFormat &dstFmt) {
-	auto const codecType = metadataDemux->type;
+IPipelinedModule* createConverter(Pipeline* pipeline, Metadata metadata, Metadata metadataEncoder, const PictureFormat &dstFmt) {
+	auto const codecType = metadata->type;
 	if (codecType == VIDEO_PKT) {
 		Log::msg(Info, "[Converter] Found video stream");
 		return pipeline->add("VideoConvert", &dstFmt);
 	} else if (codecType == AUDIO_PKT) {
 		Log::msg(Info, "[Converter] Found audio stream");
-		auto const demuxFmt = toPcmFormat(safe_cast<const MetadataPktLibavAudio>(metadataDemux));
+		auto const demuxFmt = toPcmFormat(safe_cast<const MetadataPktLibavAudio>(metadata));
 		auto const metaEnc = safe_cast<const MetadataPktLibavAudio>(metadataEncoder);
 		auto const encFmt = toPcmFormat(metaEnc);
 		auto format = PcmFormat(demuxFmt.sampleRate, demuxFmt.numChannels, demuxFmt.layout, encFmt.sampleFormat, (encFmt.numPlanes == 1) ? Interleaved : Planar);
@@ -115,7 +115,6 @@ std::unique_ptr<Pipeline> buildPipeline(const Config &cfg) {
 	auto pipeline = make_unique<Pipeline>(cfg.ultraLowLatency, cfg.ultraLowLatency ? Pipeline::Mono : Pipeline::OnePerModule);
 
 	ensureDir(cfg.workingDir);
-
 	changeDir(cfg.workingDir);
 
 	DemuxConfig demuxCfg;
@@ -137,9 +136,9 @@ std::unique_ptr<Pipeline> buildPipeline(const Config &cfg) {
 
 	int numDashInputs = 0;
 	auto processElementaryStream = [&](int streamIndex) {
-		auto const metadataDemux = demux->getOutputMetadata(streamIndex);
-		if (!metadataDemux) {
-			Log::msg(Warning, "[%s] Unknown metadataDemux for stream %s. Ignoring.", g_appName, streamIndex);
+		auto const metadata = demux->getOutputMetadata(streamIndex);
+		if (!metadata) {
+			Log::msg(Warning, "[%s] Unknown metadata for stream %s. Ignoring.", g_appName, streamIndex);
 			return;
 		}
 
@@ -154,35 +153,35 @@ std::unique_ptr<Pipeline> buildPipeline(const Config &cfg) {
 
 		IPipelinedModule *decode = nullptr;
 		if (transcode) {
-			decode = pipeline->add("Decoder", metadataDemux->type);
+			decode = pipeline->add("Decoder", metadata->type);
 
 			pipeline->connect(source, GetInputPin(decode));
 
-			if (metadataDemux->isVideo() && cfg.autoRotate) {
-				auto const res = safe_cast<const MetadataPktLibavVideo>(metadataDemux)->getResolution();
+			if (metadata->isVideo() && cfg.autoRotate) {
+				auto const res = safe_cast<const MetadataPktLibavVideo>(metadata)->getResolution();
 				if (res.height > res.width) {
 					isVertical = true;
 				}
 			}
 		}
 
-		auto const numRes = metadataDemux->isVideo() ? std::max<int>(cfg.v.size(), 1) : 1;
+		auto const numRes = metadata->isVideo() ? std::max<int>(cfg.v.size(), 1) : 1;
 		for (int r = 0; r < numRes; ++r) {
 			auto compressed = source;
 			if (transcode) {
-				auto inputRes = metadataDemux->isVideo() ? safe_cast<const MetadataPktLibavVideo>(metadataDemux)->getResolution() : Resolution();
+				auto inputRes = metadata->isVideo() ? safe_cast<const MetadataPktLibavVideo>(metadata)->getResolution() : Resolution();
 				auto const outputRes = autoRotate(autoFit(inputRes, cfg.v[r].res), isVertical);
 				PictureFormat encoderInputPicFmt(outputRes, UNKNOWN_PF);
-				auto encoder = createEncoder(pipeline.get(), metadataDemux, cfg.ultraLowLatency, (VideoCodecType)cfg.v[r].type, encoderInputPicFmt, cfg.v[r].bitrate, cfg.segmentDurationInMs);
+				auto encoder = createEncoder(pipeline.get(), metadata, cfg.ultraLowLatency, (VideoCodecType)cfg.v[r].type, encoderInputPicFmt, cfg.v[r].bitrate, cfg.segmentDurationInMs);
 				if (!encoder)
 					return;
 
-				auto converter = createConverter(pipeline.get(), metadataDemux, encoder->getOutputMetadata(0), encoderInputPicFmt);
+				auto converter = createConverter(pipeline.get(), metadata, encoder->getOutputMetadata(0), encoderInputPicFmt);
 				if (!converter)
 					return;
 
 				if(cfg.debugMonitor) {
-					if (metadataDemux->isVideo() && r == 0) {
+					if (metadata->isVideo() && r == 0) {
 						auto webcamPreview = pipeline->add("SDLVideo", nullptr);
 						pipeline->connect(converter, webcamPreview);
 					}
@@ -194,7 +193,7 @@ std::unique_ptr<Pipeline> buildPipeline(const Config &cfg) {
 			}
 
 			std::string prefix;
-			if (metadataDemux->isVideo()) {
+			if (metadata->isVideo()) {
 				auto reso = safe_cast<const MetadataPktLibavVideo>(demux->getOutputMetadata(streamIndex))->getResolution();
 				if (transcode)
 					reso = autoFit(reso, cfg.v[r].res);
@@ -221,9 +220,8 @@ std::unique_ptr<Pipeline> buildPipeline(const Config &cfg) {
 		}
 	};
 
-	for (int i = 0; i < demux->getNumOutputs(); ++i) {
+	for (int i = 0; i < demux->getNumOutputs(); ++i)
 		processElementaryStream(i);
-	}
 
 	return pipeline;
 }
