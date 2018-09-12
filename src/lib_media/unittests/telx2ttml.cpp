3f4f37cfdfcf40394b9b3237b4bb7486f6340031
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-09-12 11:55:59 +0200

    TeletextToTTML tests: start injecting data
    At the moment, send garbage data.

--- src/lib_media/unittests/telx2ttml.cpp
+++ src/lib_media/unittests/telx2ttml.cpp
@@ -1,13 +1,37 @@
 #include "tests/tests.hpp"
 #include "lib_modules/modules.hpp"
 #include "lib_modules/utils/loader.hpp"
+#include "lib_media/common/metadata.hpp"
 #include "lib_media/transform/telx2ttml.hpp"
+#include <string.h>
 
 using namespace Tests;
 using namespace Modules;
 
+namespace {
+
+template<size_t numBytes>
+std::shared_ptr<DataBase> createPacket(uint8_t const (&bytes)[numBytes]) {
+	auto pkt = make_shared<DataRaw>(numBytes);
+	memcpy(pkt->data().ptr, bytes, numBytes);
+	return pkt;
+}
+
+std::shared_ptr<DataBase> getTeletextTestFrame() {
+	static const uint8_t teletext[] = {
+		// garbage data
+		0xde, 0xad, 0xbe, 0xef, 0x4a, 0xce, 0x00, 0x00,
+	};
+
+	auto r= createPacket(teletext);
+	r->setMetadata(make_shared<MetadataPkt>(SUBTITLE_PKT));
+	return r;
+}
+}
 unittest("telx2ttml: simple") {
 	TeletextToTtmlConfig cfg;
 	auto reader = loadModule("TeletextToTTML", &NullHost, &cfg);
+	reader->getInput(0)->push(getTeletextTestFrame());
+	reader->process();
 }
 
