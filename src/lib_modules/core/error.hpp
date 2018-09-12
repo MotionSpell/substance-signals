d3b98740a55ada5eb2ec91ebe7aa806edd033257
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-09-12 09:18:27 +0200

    ErrorCap: avoid object slicing ('runtime_error' to 'std::exception')

--- src/lib_modules/core/error.hpp
+++ src/lib_modules/core/error.hpp
@@ -7,7 +7,7 @@ namespace Modules {
 
 class ErrorCap {
 	protected:
-		std::exception error(std::string const &msg) const {
+		std::runtime_error error(std::string const &msg) const {
 			return std::runtime_error("[" + std::string(typeid(*this).name()) + "]" + msg);
 		}
 };
