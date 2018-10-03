e514bb8c10b8d49411d850ba88184611da657b81
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-10-03 16:45:16 +0200

    HTTP tests: submit real data

--- src/lib_media/unittests/http.cpp
+++ src/lib_media/unittests/http.cpp
@@ -2,12 +2,26 @@
 #include "lib_media/out/http.hpp"
 #include "lib_modules/utils/loader.hpp"
 #include "lib_modules/modules.hpp"
+#include <string.h> // memcpy
 
 using namespace Modules;
+
+namespace {
+
+std::shared_ptr<DataBase> createPacket(span<const char> contents) {
+	auto r = make_shared<DataRaw>(contents.len);
+	memcpy(r->data().ptr, contents.ptr, contents.len);
+	return r;
+}
+
+}
 
 secondclasstest("HTTP post") {
 	HttpOutputConfig cfg {};
 	cfg.url = "http://example.com";
-	auto demux = loadModule("HTTP", &NullHost, &cfg);
+	auto mod = loadModule("HTTP", &NullHost, &cfg);
+	mod->getInput(0)->push(createPacket("Hello World"));
+	mod->process();
+	mod->flush();
 }
 
