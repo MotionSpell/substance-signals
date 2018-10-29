551337e24119b623d07aa77468753a26d2b6b3d8
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-10-29 14:45:10 +0100

    GPACDemuxMp4: flatten class

--- src/lib_media/demux/gpac_demux_mp4_simple.cpp
+++ src/lib_media/demux/gpac_demux_mp4_simple.cpp
@@ -9,20 +9,6 @@ using namespace Modules;
 
 namespace {
 
-class ISOFileReader;
-
-class GPACDemuxMP4Simple : public ActiveModule {
-	public:
-		GPACDemuxMP4Simple(IModuleHost* host, Mp4DemuxConfig const* cfg);
-		~GPACDemuxMP4Simple();
-		bool work() override;
-
-	private:
-		IModuleHost* const m_host;
-		std::unique_ptr<ISOFileReader> reader;
-		OutputDefault* output;
-};
-
 class ISOFileReader {
 	public:
 		void init(GF_ISOFile* m) {
@@ -38,47 +24,53 @@ class ISOFileReader {
 		uint32_t sampleIndex, sampleCount;
 };
 
-GPACDemuxMP4Simple::GPACDemuxMP4Simple(IModuleHost* host, Mp4DemuxConfig const* cfg)
-	: m_host(host),
-	  reader(new ISOFileReader) {
-	GF_ISOFile *movie;
-	u64 missingBytes;
-	GF_Err e = gf_isom_open_progressive(cfg->path.c_str(), 0, 0, &movie, &missingBytes);
-	if ((e != GF_OK && e != GF_ISOM_INCOMPLETE_FILE) || movie == nullptr) {
-		throw error(format("Could not open file %s for reading (%s).", cfg->path, gf_error_to_string(e)));
-	}
-	reader->init(movie);
-	output = addOutput<OutputDefault>();
-}
 
-GPACDemuxMP4Simple::~GPACDemuxMP4Simple() {
-}
-
-bool GPACDemuxMP4Simple::work() {
-	auto const DTSOffset = reader->movie->getDTSOffset(reader->trackNumber);
-	try {
-		int sampleDescriptionIndex;
-		std::unique_ptr<gpacpp::IsoSample> ISOSample = reader->movie->getSample(reader->trackNumber, reader->sampleIndex, sampleDescriptionIndex);
-
-		m_host->log(Debug, format("Found sample #%s/%s of length %s, RAP %s, DTS: %s, CTS: %s",
-		        reader->sampleIndex, reader->sampleCount, ISOSample->dataLength,
-		        ISOSample->IsRAP, ISOSample->DTS + DTSOffset, ISOSample->DTS + DTSOffset + ISOSample->CTS_Offset).c_str());
-		reader->sampleIndex++;
-
-		auto out = output->getBuffer(ISOSample->dataLength);
-		memcpy(out->data().ptr, ISOSample->data, ISOSample->dataLength);
-		out->setMediaTime(ISOSample->DTS + DTSOffset + ISOSample->CTS_Offset, reader->movie->getMediaTimescale(reader->trackNumber));
-		output->emit(out);
-	} catch (gpacpp::Error const& err) {
-		if (err.error_ == GF_ISOM_INCOMPLETE_FILE) {
-			u64 missingBytes = reader->movie->getMissingBytes(reader->trackNumber);
-			m_host->log(Error, format("Missing %s bytes on input file", missingBytes).c_str());
-		} else {
-			return false;
+class GPACDemuxMP4Simple : public ActiveModule {
+	public:
+		GPACDemuxMP4Simple(IModuleHost* host, Mp4DemuxConfig const* cfg)
+			: m_host(host),
+			  reader(new ISOFileReader) {
+			GF_ISOFile *movie;
+			u64 missingBytes;
+			GF_Err e = gf_isom_open_progressive(cfg->path.c_str(), 0, 0, &movie, &missingBytes);
+			if ((e != GF_OK && e != GF_ISOM_INCOMPLETE_FILE) || movie == nullptr) {
+				throw error(format("Could not open file %s for reading (%s).", cfg->path, gf_error_to_string(e)));
+			}
+			reader->init(movie);
+			output = addOutput<OutputDefault>();
 		}
-	}
-	return true;
-}
+
+		bool work() override {
+			auto const DTSOffset = reader->movie->getDTSOffset(reader->trackNumber);
+			try {
+				int sampleDescriptionIndex;
+				std::unique_ptr<gpacpp::IsoSample> ISOSample = reader->movie->getSample(reader->trackNumber, reader->sampleIndex, sampleDescriptionIndex);
+
+				m_host->log(Debug, format("Found sample #%s/%s of length %s, RAP %s, DTS: %s, CTS: %s",
+				        reader->sampleIndex, reader->sampleCount, ISOSample->dataLength,
+				        ISOSample->IsRAP, ISOSample->DTS + DTSOffset, ISOSample->DTS + DTSOffset + ISOSample->CTS_Offset).c_str());
+				reader->sampleIndex++;
+
+				auto out = output->getBuffer(ISOSample->dataLength);
+				memcpy(out->data().ptr, ISOSample->data, ISOSample->dataLength);
+				out->setMediaTime(ISOSample->DTS + DTSOffset + ISOSample->CTS_Offset, reader->movie->getMediaTimescale(reader->trackNumber));
+				output->emit(out);
+			} catch (gpacpp::Error const& err) {
+				if (err.error_ == GF_ISOM_INCOMPLETE_FILE) {
+					u64 missingBytes = reader->movie->getMissingBytes(reader->trackNumber);
+					m_host->log(Error, format("Missing %s bytes on input file", missingBytes).c_str());
+				} else {
+					return false;
+				}
+			}
+			return true;
+		}
+
+	private:
+		IModuleHost* const m_host;
+		std::unique_ptr<ISOFileReader> reader;
+		OutputDefault* output;
+};
 
 
 Modules::IModule* createObject(IModuleHost* host, va_list va) {
