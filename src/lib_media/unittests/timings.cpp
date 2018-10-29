d19ff00e9e6bdc8650b93e54e3bdc7a52dc54ed0
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-10-29 15:48:38 +0100

    Timing tests: don't re-use the picture we just passed to 'push'

--- src/lib_media/unittests/timings.cpp
+++ src/lib_media/unittests/timings.cpp
@@ -20,11 +20,11 @@ std::vector<int64_t> runMux(CreateDemuxFunc createDemux, int numBFrame, const st
 	EncoderConfig p { EncoderConfig::Video };
 	p.frameRate.num = 1;
 	p.avcodecCustom = format("-bf %s", numBFrame);
-	auto picture = make_shared<PictureYUV420P>(Resolution(64, 64));
 	auto encode = loadModule("Encoder", &NullHost, &p);
 	ConnectOutputToInput(encode->getOutput(0), mux->getInput(0));
 
 	for(auto time : timesIn) {
+		auto picture = make_shared<PictureYUV420P>(Resolution(64, 64));
 		picture->setMediaTime(time);
 		encode->getInput(0)->push(picture);
 		encode->process();
