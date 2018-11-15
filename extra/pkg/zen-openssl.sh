b4f156e03807c5b87f02441ffe350b661a285452
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-11-15 14:52:33 +0100

    Replace OpenSSL with LibreSSL. Fixes a lot of build issues by using autoconf

--- extra/pkg/zen-openssl.sh
+++ extra/pkg/zen-openssl.sh
@@ -1,45 +1,9 @@
 
 function openssl_build {
-  lazy_download "openssl.tar.gz" https://www.openssl.org/source/openssl-1.1.1.tar.gz
-  lazy_extract "openssl.tar.gz"
+  lazy_download "libressl.tar.gz" https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.8.2.tar.gz
+  lazy_extract "libressl.tar.gz"
 
-  mkdir -p openssl/bin/$host
-  pushDir openssl/bin/$host
-
-  # OpenSSL uses its own OS naming scheme
-  # and doesn't support standard compiler prefix
-  case $host in
-    x86_64-*-gnu)
-      openssl_os="linux-x86_64"
-      ;;
-    i686-*-gnu)
-      openssl_os="linux-x86"
-      ;;
-    x86_64-*-mingw32)
-      openssl_os="mingw64"
-      ;;
-    i686-*-mingw32)
-      openssl_os="mingw"
-      ;;
-    x86_64-apple-darwin*)
-      openssl_os="darwin64-x86_64-cc"
-      ;;
-    *)
-      echo "ERROR: platform not implemented: $host" >&2
-      exit 1
-  esac
-
-  ../../Configure \
-    $openssl_os \
-    shared \
-    no-unit-test \
-    no-tests \
-    --cross-compile-prefix="$host-" \
-    --prefix=$PREFIX
-  $MAKE depend
-  $MAKE
-  $MAKE install_dev install_runtime
-  popDir
+  autoconf_build $host "libressl"
 }
 
 function openssl_get_deps {
