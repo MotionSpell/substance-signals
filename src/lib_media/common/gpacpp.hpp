f68d82c48a60fa20bd52bdac5e76427c3ca4ef0d
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2018-11-25 18:45:26 +0100

    GPAC DASH MPD serialization: don't use not portable tmpfile() and improve error messages

--- src/lib_media/common/gpacpp.hpp
+++ src/lib_media/common/gpacpp.hpp
@@ -254,20 +254,22 @@ class MPD {
 		}
 
 		std::string serialize() const {
-			auto fp = std::shared_ptr<FILE>(tmpfile(), &fclose);
+			char *pfn = nullptr;
+			auto fp = std::shared_ptr<FILE>(gf_temp_file_new(&pfn), &fclose);
+			auto fn = std::shared_ptr<char>(pfn, &gf_free);
 			if(!fp)
-				throw Error(format("[MPEG-DASH MPD] Can't serialize manifest (2)").c_str(), GF_IO_ERR);
+				throw Error(format("[MPEG-DASH MPD] Can't serialize manifest (2): %s", fn.get()).c_str(), GF_IO_ERR);
 
 			GF_Err e = gf_mpd_write(mpd, fp.get());
 			if (e != GF_OK)
-				throw Error(format("[MPEG-DASH MPD] Can't serialize manifest (1)").c_str(), e);
+				throw Error(format("[MPEG-DASH MPD] Can't serialize manifest (1): %s", fn, fn.get()).c_str(), e);
 
 			std::string r;
 			r.resize(ftell(fp.get()));
 			fseek(fp.get(), 0, SEEK_SET);
 			auto nread = fread(&r[0], 1, r.size(), fp.get());
 			if(nread != r.size())
-				throw Error(format("[MPEG-DASH MPD] Can't read serialized manifest").c_str(), e);
+				throw Error(format("[MPEG-DASH MPD] Can't read serialized manifest (read %s out of %s bytes from \"%s\")", nread, r.size(), fn.get()).c_str(), GF_IO_ERR);
 
 			return r;
 		}
