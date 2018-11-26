f330b6d735aed01b5d907225f1629e57fcd65100
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-11-26 10:50:45 +0100

    Remove obsolete MSVC-specific code

--- src/lib_appcommon/safemain.cpp
+++ src/lib_appcommon/safemain.cpp
@@ -21,27 +21,6 @@ static void onInterruption() {
 	}
 }
 
-#ifdef _MSC_VER
-#include <Windows.h>
-static BOOL WINAPI signalHandler(_In_ DWORD dwCtrlType) {
-	switch (dwCtrlType) {
-	case CTRL_C_EVENT:
-	case CTRL_BREAK_EVENT:
-	case CTRL_CLOSE_EVENT:
-	case CTRL_LOGOFF_EVENT:
-	case CTRL_SHUTDOWN_EVENT:
-		onInterruption();
-		return TRUE;
-	default:
-		return FALSE;
-	}
-}
-
-static void installSignalHandler() {
-	SetConsoleCtrlHandler(signalHandler, TRUE);
-}
-
-#else
 static void sigTermHandler(int sig) {
 	switch (sig) {
 	case SIGINT:
@@ -58,8 +37,6 @@ static void installSignalHandler() {
 	std::signal(SIGINT, sigTermHandler);
 	std::signal(SIGTERM, sigTermHandler);
 }
-
-#endif
 
 int main(int argc, char const* argv[]) {
 	try {
