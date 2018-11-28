cf25a7a72bd78aae4ada4c5c3239804948d9f818
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-11-28 13:56:28 +0100

    Makefile: simplify

--- src/lib_media/render/render.mk
+++ src/lib_media/render/render.mk
@@ -9,12 +9,12 @@ endif
 TARGETS+=$(BIN)/SDLVideo.smd
 $(BIN)/SDLVideo.smd: $(BIN)/$(SRC)/lib_media/render/sdl_video.cpp.o
 $(BIN)/SDLVideo.smd: LDFLAGS+=$(RENDER_LDFLAGS)
-$(BIN)/$(SRC)/lib_media/render/sdl_video.cpp.o: CFLAGS+=$(RENDER_CFLAGS)
+$(BIN)/SDLVideo.smd: CFLAGS+=$(RENDER_CFLAGS)
 
 TARGETS+=$(BIN)/SDLAudio.smd
 $(BIN)/SDLAudio.smd: \
 	$(BIN)/$(SRC)/lib_media/render/sdl_audio.cpp.o \
 	$(BIN)/$(SRC)/lib_media/common/libav.cpp.o
 $(BIN)/SDLAudio.smd: LDFLAGS+=$(RENDER_LDFLAGS)
-$(BIN)/$(SRC)/lib_media/render/sdl_audio.cpp.o: CFLAGS+=$(RENDER_CFLAGS)
+$(BIN)/SDLAudio.smd: CFLAGS+=$(RENDER_CFLAGS)
 
