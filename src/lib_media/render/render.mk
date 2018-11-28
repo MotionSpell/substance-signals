802b3703b66daa7db2ca8e23eccc4d8fa5f51fd7
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-11-28 13:57:56 +0100

    Makefile: cosmetics

--- src/lib_media/render/render.mk
+++ src/lib_media/render/render.mk
@@ -7,14 +7,15 @@ include $(BIN)/render-config.mk
 endif
 
 TARGETS+=$(BIN)/SDLVideo.smd
-$(BIN)/SDLVideo.smd: $(BIN)/$(SRC)/lib_media/render/sdl_video.cpp.o
 $(BIN)/SDLVideo.smd: LDFLAGS+=$(RENDER_LDFLAGS)
 $(BIN)/SDLVideo.smd: CFLAGS+=$(RENDER_CFLAGS)
+$(BIN)/SDLVideo.smd: \
+  $(BIN)/$(SRC)/lib_media/render/sdl_video.cpp.o
 
 TARGETS+=$(BIN)/SDLAudio.smd
-$(BIN)/SDLAudio.smd: \
-	$(BIN)/$(SRC)/lib_media/render/sdl_audio.cpp.o \
-	$(BIN)/$(SRC)/lib_media/common/libav.cpp.o
 $(BIN)/SDLAudio.smd: LDFLAGS+=$(RENDER_LDFLAGS)
 $(BIN)/SDLAudio.smd: CFLAGS+=$(RENDER_CFLAGS)
+$(BIN)/SDLAudio.smd: \
+  $(BIN)/$(SRC)/lib_media/render/sdl_audio.cpp.o \
+  $(BIN)/$(SRC)/lib_media/common/libav.cpp.o
 
