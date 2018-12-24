7b2b73d2276d0f37d4b865e6741e69ef4979ffe8
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-12-24 09:12:52 +0100

    Only use SDL cflags for source files actually using SDL

--- src/lib_media/render/render.mk
+++ src/lib_media/render/render.mk
@@ -7,14 +7,14 @@ include $(BIN)/render-config.mk
 endif
 
 TARGETS+=$(BIN)/SDLVideo.smd
+$(BIN)/$(SRC)/lib_media/render/sdl_video.cpp.o: CFLAGS+=$(RENDER_CFLAGS)
 $(BIN)/SDLVideo.smd: LDFLAGS+=$(RENDER_LDFLAGS)
-$(BIN)/SDLVideo.smd: CFLAGS+=$(RENDER_CFLAGS)
 $(BIN)/SDLVideo.smd: \
   $(BIN)/$(SRC)/lib_media/render/sdl_video.cpp.o
 
 TARGETS+=$(BIN)/SDLAudio.smd
+$(BIN)/$(SRC)/lib_media/render/sdl_audio.cpp.o: CFLAGS+=$(RENDER_CFLAGS)
 $(BIN)/SDLAudio.smd: LDFLAGS+=$(RENDER_LDFLAGS)
-$(BIN)/SDLAudio.smd: CFLAGS+=$(RENDER_CFLAGS)
 $(BIN)/SDLAudio.smd: \
   $(BIN)/$(SRC)/lib_media/render/sdl_audio.cpp.o \
   $(BIN)/$(SRC)/lib_media/common/libav.cpp.o
