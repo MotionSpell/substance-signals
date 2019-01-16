f87aa31fefe62e629ab6d83da9d3ece694963513
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-01-16 15:06:17 +0100

    Video generator: print 'SIGNALS' at the center of the frame

--- src/lib_media/in/video_generator.cpp
+++ src/lib_media/in/video_generator.cpp
@@ -7,11 +7,12 @@ namespace {
 
 span<const char*> getFont();
 
-void renderDigit(uint8_t* dst, size_t stride, int digitValue) {
-	assert(digitValue >=0 && digitValue <= 9);
-
+void renderChar(uint8_t* dst, size_t stride, int asciiValue) {
 	auto font = getFont();
-	auto src = font[digitValue];
+
+	assert(asciiValue >=0 && asciiValue <= (int)font.len);
+
+	auto src = font[asciiValue];
 
 	for(int y=0; y < 5; ++y) {
 		for(int x=0; x < 5; ++x) {
@@ -23,20 +24,20 @@ void renderDigit(uint8_t* dst, size_t stride, int digitValue) {
 	}
 }
 
-void renderNumber(uint8_t* dst, size_t stride, int value) {
-	auto const margin = 3;
+void renderString(uint8_t* dst, size_t stride, const char* buffer) {
 	auto const hstep = 6;
-	dst += margin;
-	dst += stride * margin;
+	int i=0;
+	while(buffer[i]) {
+		renderChar(dst, stride, buffer[i]);
+		dst += hstep;
+		++i;
+	}
+}
 
-	renderDigit(dst, stride, (value/100)%10);
-	dst += hstep;
-
-	renderDigit(dst, stride, (value/10)%10);
-	dst += hstep;
-
-	renderDigit(dst, stride, (value/1)%10);
-	dst += hstep;
+void renderNumber(uint8_t* dst, size_t stride, int value) {
+	char buffer[256];
+	sprintf(buffer, "%03d", value);
+	renderString(dst, stride, buffer);
 }
 
 }
@@ -67,8 +68,28 @@ void VideoGenerator::process() {
 	auto const val = flash ? 0xCC : 0x80;
 	memset(p, val, pic->getSize());
 
-	if(dim.width > 32 && dim.height > 32)
-		renderNumber(pic->getPlane(0), pic->getStride(0), m_numFrames);
+	if(dim.width > 32 && dim.height > 32) {
+
+		auto dst = pic->getPlane(0);
+		auto stride = pic->getStride(0);
+		auto const margin = 3;
+		dst += margin;
+		dst += stride * margin;
+
+		renderNumber(dst, stride, m_numFrames);
+	}
+
+	if(dim.width > 64 && dim.height > 32) {
+
+		auto dst = pic->getPlane(0);
+		auto stride = pic->getStride(0);
+		auto const h_margin = (dim.width - 6 * 7) / 2;
+		auto const v_margin = dim.height/ 2;
+		dst += h_margin;
+		dst += stride * v_margin;
+
+		renderString(dst, stride, "SIGNALS");
+	}
 
 	auto const framePeriodIn180k = IClock::Rate / m_frameRate;
 	assert(IClock::Rate % m_frameRate == 0);
@@ -85,6 +106,90 @@ void VideoGenerator::process() {
 namespace {
 span<const char*> getFont() {
 	static const char* font[] = {
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+
+		// space
+		".........................",
+
+		// bang
+		"..X.."
+		"..X.."
+		"..X.."
+		"....."
+		"..X..",
+
+		// double-quote
+		".X.X."
+		".X.X."
+		"....."
+		"....."
+		".....",
+
+		// hash
+		".X.X."
+		"XXXXX"
+		".X.X."
+		"XXXXX"
+		".X.X.",
+
+		// dollar
+		".XXX."
+		"X.X.."
+		".XXX."
+		"..X.X"
+		".XXX.",
+
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+
+		"....X"
+		"...X."
+		"..X.."
+		".X..."
+		"X....",
+
 		".XXX."
 		"X...X"
 		"X...X"
@@ -144,6 +249,116 @@ span<const char*> getFont() {
 		".XXXX"
 		"....X"
 		".XXX.",
+
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+
+		// @
+		".XXX."
+		"X.X.X"
+		"X.XXX"
+		"X...."
+		".XXX.",
+
+		".XXX."
+		"X...X"
+		"XXXXX"
+		"X...X"
+		"X...X",
+
+		"XXXX."
+		"X...X"
+		"XXXX."
+		"X...X"
+		"XXXXX",
+
+		".XXXX"
+		"X...."
+		"X...."
+		"X...."
+		".XXXX",
+
+		"XXXX."
+		"X...X"
+		"X...X"
+		"X...X"
+		"XXXX.",
+
+		"XXXXX"
+		"X...."
+		"XXX.."
+		"X...."
+		"XXXXX",
+
+		"XXXXX"
+		"X...."
+		"XXX.."
+		"X...."
+		"X....",
+
+		".XXXX"
+		"X...."
+		"X..XX"
+		"X...X"
+		".XXXX",
+
+		"X...X"
+		"X...X"
+		"XXXXX"
+		"X...X"
+		"X...X",
+
+		"XXXXX"
+		"..X.."
+		"..X.."
+		"..X.."
+		"XXXXX",
+
+		"XXXXX"
+		"...X."
+		"...X."
+		"...X."
+		"XXX..",
+
+		"X..X."
+		"X.X.."
+		"XX..."
+		"X.X.."
+		"X..X.",
+
+		"X...."
+		"X...."
+		"X...."
+		"X...."
+		"XXXXX",
+
+		"X...X"
+		"XX.XX"
+		"X.X.X"
+		"X...X"
+		"X...X",
+
+		"X...X"
+		"XX..X"
+		"X.X.X"
+		"X..XX"
+		"X...X",
+
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+		"XXXXXXXXXXXXXXXXXXXXXXXXX",
+
+		"XXXXX"
+		"X...."
+		".XXX."
+		"....X"
+		"XXXXX",
+
 	};
 
 	return font;
