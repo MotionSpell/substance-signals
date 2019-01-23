dac5cea6c6563c7cb0e750728cae69d9930838fd
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-01-23 11:21:34 +0100

    Pipeline: add a (failing) unit test that shows a crash at pipeline destruction. Disabled for the moment.

--- src/lib_pipeline/unittests/pipeline_interrupt.cpp
+++ src/lib_pipeline/unittests/pipeline_interrupt.cpp
@@ -21,11 +21,37 @@ unittest("pipeline: EOS injection (exitSync)") {
 	tf.join();
 }
 
-unittest("pipeline: destroy while running") {
+unittest("pipeline: destroy while running: easy") {
 	auto p = std::make_unique<Pipeline>();
 	auto src = p->addModule<InfiniteSource>();
 	auto sink = p->addModule<FakeSink>();
 	p->connect(src, sink);
 	p->start();
+}
+
+struct FakeTransformer : public Modules::ModuleS {
+	FakeTransformer(Modules::KHost*) {
+		addInput(this);
+		out = addOutput<Modules::OutputDefault>();
+	}
+	void process(Modules::Data) override {
+		out->post(out->getBuffer(0));
+	}
+	Modules::OutputDefault* out;
+};
+
+unittest("[DISABLED] pipeline: destroy while running: long chain of modules") {
+	auto p = std::make_unique<Pipeline>();
+	auto src = p->addModule<InfiniteSource>();
+	auto trans1 = p->addModule<FakeTransformer>();
+	auto trans2 = p->addModule<FakeTransformer>();
+	auto trans3 = p->addModule<FakeTransformer>();
+	auto sink = p->addModule<FakeSink>();
+	p->connect(src, trans1);
+	p->connect(trans1, trans2);
+	p->connect(trans2, trans3);
+	p->connect(trans3, sink);
+	p->start();
+	std::this_thread::sleep_for(std::chrono::milliseconds(1));
 }
 
