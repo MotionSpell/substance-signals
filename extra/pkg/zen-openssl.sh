bdacf35f56f117a710e3210af3cfa0b178c896a8
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-01-23 13:10:42 +0100

    zen-openssl: don't build manpages
    This fixes a failure at 'make install' time on MSYS2.

--- extra/pkg/zen-openssl.sh
+++ extra/pkg/zen-openssl.sh
@@ -3,6 +3,10 @@ function openssl_build {
   lazy_download "libressl.tar.gz" https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-2.8.2.tar.gz
   lazy_extract "libressl.tar.gz"
 
+  # Don't build manpages.
+  # We don't use them, it's slow on all platforms, and it fails under MSYS2.
+  sed 's/^SUBDIRS = \(.*\) man/SUBDIRS = \1/' -i libressl/Makefile.in
+
   autoconf_build $host "libressl"
 }
 
