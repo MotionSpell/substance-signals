b4b332f764e43f3566cebc6e0a7d60ddf07a7959
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-04-05 17:30:35 +0200

    Flatten class: JPEGTurboEncode

--- src/lib_media/encode/jpegturbo_encode.cpp
+++ src/lib_media/encode/jpegturbo_encode.cpp
@@ -17,9 +17,65 @@ namespace {
 
 class JPEGTurboEncode : public ModuleS {
 	public:
-		JPEGTurboEncode(KHost* host, int quality = JPEG_DEFAULT_QUALITY);
-		~JPEGTurboEncode();
-		void processOne(Data data) override;
+		JPEGTurboEncode(KHost* host_, int quality = JPEG_DEFAULT_QUALITY)
+			: m_host(host_),
+			  jtHandle(tjInitCompress()), quality(quality) {
+			input->setMetadata(make_shared<MetadataRawVideo>());
+			output = addOutput<OutputDefault>();
+		}
+
+		~JPEGTurboEncode() {
+			tjDestroy(jtHandle);
+		}
+
+		void processOne(Data data_) override {
+			auto videoData = safe_cast<const DataPicture>(data_);
+			auto const w = videoData->getFormat().res.width, h = videoData->getFormat().res.height;
+			auto const dataSize = tjBufSize(w, h, TJSAMP_420);
+			auto out = output->allocData<DataRaw>(dataSize);
+			unsigned char *buf = (unsigned char*)out->data().ptr;
+			auto jpegBuf = videoData->data().ptr;
+			unsigned long jpegSize;
+
+			switch (videoData->getFormat().format) {
+			case PixelFormat::Y8:
+			case PixelFormat::I420:
+			case PixelFormat::YUV420P10LE:
+			case PixelFormat::YUV422P:
+			case PixelFormat::YUV422P10LE:
+			case PixelFormat::YUYV422:
+			case PixelFormat::NV12: {
+				uint8_t const* srcSlice[8] {};
+				int srcStride[8] {};
+				for (int i = 0; i<videoData->getNumPlanes(); ++i) {
+					srcSlice[i] = videoData->getPlane(i);
+					srcStride[i] = (int)videoData->getStride(i);
+				}
+
+				if (tjCompressFromYUVPlanes(jtHandle,
+				        srcSlice, videoData->getFormat().res.width, srcStride, videoData->getFormat().res.height, TJSAMP_420,
+				        &buf, &jpegSize, quality, TJFLAG_NOREALLOC | TJFLAG_FASTDCT)) {
+					m_host->log(Warning, "error encountered while compressing (YUV).");
+					return;
+				}
+				break;
+			}
+			case PixelFormat::RGB24:
+			case PixelFormat::RGBA32: {
+				if (tjCompress2(jtHandle, (unsigned char*)jpegBuf, w, 0/*pitch*/, h, TJPF_RGB, &buf, &jpegSize, TJSAMP_420, quality, TJFLAG_NOREALLOC | TJFLAG_FASTDCT) < 0) {
+					m_host->log(Warning, "error encountered while compressing (RGB).");
+					return;
+				}
+				break;
+			}
+			default:
+				throw error(format("Unsupported colorspace %s", (int)videoData->getFormat().format));
+			}
+
+			out->buffer->resize(jpegSize);
+			out->setMediaTime(data_->getMediaTime());
+			output->post(out);
+		}
 
 	private:
 		KHost* const m_host;
@@ -27,66 +83,6 @@ class JPEGTurboEncode : public ModuleS {
 		tjhandle jtHandle;
 		int quality;
 };
-
-JPEGTurboEncode::JPEGTurboEncode(KHost* host_, int quality)
-	: m_host(host_),
-	  jtHandle(tjInitCompress()), quality(quality) {
-	input->setMetadata(make_shared<MetadataRawVideo>());
-	output = addOutput<OutputDefault>();
-}
-
-JPEGTurboEncode::~JPEGTurboEncode() {
-	tjDestroy(jtHandle);
-}
-
-void JPEGTurboEncode::processOne(Data data_) {
-	auto videoData = safe_cast<const DataPicture>(data_);
-	auto const w = videoData->getFormat().res.width, h = videoData->getFormat().res.height;
-	auto const dataSize = tjBufSize(w, h, TJSAMP_420);
-	auto out = output->allocData<DataRaw>(dataSize);
-	unsigned char *buf = (unsigned char*)out->data().ptr;
-	auto jpegBuf = videoData->data().ptr;
-	unsigned long jpegSize;
-
-	switch (videoData->getFormat().format) {
-	case PixelFormat::Y8:
-	case PixelFormat::I420:
-	case PixelFormat::YUV420P10LE:
-	case PixelFormat::YUV422P:
-	case PixelFormat::YUV422P10LE:
-	case PixelFormat::YUYV422:
-	case PixelFormat::NV12: {
-		uint8_t const* srcSlice[8] {};
-		int srcStride[8] {};
-		for (int i = 0; i<videoData->getNumPlanes(); ++i) {
-			srcSlice[i] = videoData->getPlane(i);
-			srcStride[i] = (int)videoData->getStride(i);
-		}
-
-		if (tjCompressFromYUVPlanes(jtHandle,
-		        srcSlice, videoData->getFormat().res.width, srcStride, videoData->getFormat().res.height, TJSAMP_420,
-		        &buf, &jpegSize, quality, TJFLAG_NOREALLOC | TJFLAG_FASTDCT)) {
-			m_host->log(Warning, "error encountered while compressing (YUV).");
-			return;
-		}
-		break;
-	}
-	case PixelFormat::RGB24:
-	case PixelFormat::RGBA32: {
-		if (tjCompress2(jtHandle, (unsigned char*)jpegBuf, w, 0/*pitch*/, h, TJPF_RGB, &buf, &jpegSize, TJSAMP_420, quality, TJFLAG_NOREALLOC | TJFLAG_FASTDCT) < 0) {
-			m_host->log(Warning, "error encountered while compressing (RGB).");
-			return;
-		}
-		break;
-	}
-	default:
-		throw error(format("Unsupported colorspace %s", (int)videoData->getFormat().format));
-	}
-
-	out->buffer->resize(jpegSize);
-	out->setMediaTime(data_->getMediaTime());
-	output->post(out);
-}
 
 IModule* createObject(KHost* host, void* va) {
 	(void)va;
