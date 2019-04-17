62c3c067aa57c382a42420cb73f4e41266046b6e
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-04-17 10:25:04 +0200

    SdlRender: don't use 'configure' script

--- src/plugins/SdlRender/render.mk
+++ src/plugins/SdlRender/render.mk
@@ -1,20 +1,11 @@
-$(BIN)/render-config.mk: $(SRC)/../scripts/configure
-	@mkdir -p $(BIN)
-	$(SRC)/../scripts/configure --scope RENDER_ sdl2 > "$@"
-
-ifneq ($(MAKECMDGOALS),clean)
-include $(BIN)/render-config.mk
-endif
 
 TARGETS+=$(BIN)/SDLVideo.smd
-$(BIN)/$(SRC)/plugins/SdlRender/sdl_video.cpp.o: CFLAGS+=$(RENDER_CFLAGS)
-$(BIN)/SDLVideo.smd: LDFLAGS+=$(RENDER_LDFLAGS)
+$(BIN)/SDLVideo.smd: PKGS+=sdl2
 $(BIN)/SDLVideo.smd: \
   $(BIN)/$(SRC)/plugins/SdlRender/sdl_video.cpp.o
 
 TARGETS+=$(BIN)/SDLAudio.smd
-$(BIN)/$(SRC)/plugins/SdlRender/sdl_audio.cpp.o: CFLAGS+=$(RENDER_CFLAGS)
-$(BIN)/SDLAudio.smd: LDFLAGS+=$(RENDER_LDFLAGS)
+$(BIN)/SDLAudio.smd: PKGS+=sdl2
 $(BIN)/SDLAudio.smd: \
   $(BIN)/$(SRC)/plugins/SdlRender/sdl_audio.cpp.o \
 
