11c75251dfff12afeeb2425b738f1bda1a77fcf8
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-04-23 19:10:16 +0200

    Dead code elimination

--- src/lib_signals/executor_threadpool.hpp
+++ src/lib_signals/executor_threadpool.hpp
@@ -5,23 +5,6 @@
 
 namespace Signals {
 
-//tasks occur in the pool
-class ExecutorThreadPool : public IExecutor {
-	public:
-		ExecutorThreadPool() : threadPool(std::make_shared<ThreadPool>()) {
-		}
-
-		ExecutorThreadPool(std::shared_ptr<ThreadPool> threadPool) : threadPool(threadPool) {
-		}
-
-		void operator() (const std::function<void()> &fn) {
-			threadPool->submit(fn);
-		}
-
-	private:
-		std::shared_ptr<ThreadPool> threadPool;
-};
-
 //tasks occur in a thread
 class ExecutorThread : public IExecutor {
 	public:
