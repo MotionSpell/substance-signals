e3866b1f037bc7b6dde9cc249d67bb0942eef981
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-06-06 17:23:08 +0200

    Add first unit test for Dasher. Only does blind instanciation for the moment

---
+++ src/lib_media/unittests/mpeg_dash.cpp
@@ -0,0 +1,32 @@
+#include "tests/tests.hpp"
+#include "lib_modules/modules.hpp"
+#include "lib_modules/utils/loader.hpp"
+#include "lib_media/common/attributes.hpp"
+#include "lib_media/common/metadata.hpp"
+#include "lib_media/stream/mpeg_dash.hpp" // DasherConfig
+
+using namespace Tests;
+using namespace Modules;
+using namespace std;
+
+unittest("dasher: simple") {
+
+	struct MyOutput : ModuleS {
+		void processOne(Data) override {
+			++frameCount;
+		}
+		int frameCount = 0;
+	};
+
+	DasherConfig cfg {};
+	cfg.segDurationInMs = 3000;
+	auto dasher = loadModule("MPEG_DASH", &NullHost, &cfg);
+	auto rec = createModule<MyOutput>();
+	ConnectOutputToInput(dasher->getOutput(0), rec->getInput(0));
+
+	dasher->flush();
+
+	ASSERT_EQUALS(0, rec->frameCount);
+}
+
+
