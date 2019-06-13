02c4148b03ae61b48506058570a5564f5a24d246
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-06-13 13:45:28 +0200

    Dasher tests: add a test to show that timeshift segment deletion works

--- src/plugins/Dasher/unittests/mpeg_dash.cpp
+++ src/plugins/Dasher/unittests/mpeg_dash.cpp
@@ -3,6 +3,7 @@
 #include "lib_modules/utils/loader.hpp"
 #include "lib_media/common/attributes.hpp"
 #include "lib_media/common/metadata_file.hpp"
+#include "lib_utils/tools.hpp" // safe_cast
 #include "plugins/Dasher/mpeg_dash.hpp" // DasherConfig
 
 using namespace Tests;
@@ -98,5 +99,39 @@ unittest("dasher: two live streams") {
 
 	// All the segments must have been received
 	ASSERT_EQUALS(50, recSeg->frameCount);
+}
+
+unittest("dasher: timeshift buffer") {
+
+	struct Server : ModuleS {
+		void processOne(Data data) override {
+			auto meta = safe_cast<const MetadataFile>(data->getMetadata().get());
+			if(meta->filesize == INT64_MAX) // is it a "DELETE" ?
+				deleted ++;
+			else
+				added ++;
+		}
+		int added = 0;
+		int deleted = 0;
+	};
+
+	DasherConfig cfg {};
+	cfg.segDurationInMs = 3000;
+	cfg.timeShiftBufferDepthInMs = 9000;
+	cfg.live = true;
+	auto dasher = loadModule("MPEG_DASH", &NullHost, &cfg);
+
+	auto server = createModule<Server>();
+	ConnectOutputToInput(dasher->getOutput(0), server->getInput(0));
+
+	dasher->getInput(0)->connect();
+
+	for(int i=0; i < 40; ++i) {
+		auto s = getTestSegment();
+		dasher->getInput(0)->push(s);
+		dasher->process();
+	}
+	ASSERT_EQUALS(40, server->added);
+	ASSERT_EQUALS(36, server->deleted);
 }
 
