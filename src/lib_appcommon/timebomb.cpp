a55d4ca82f21c08d8b31ff8e2667613d63307b10
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-08-27 15:47:56 +0200

    auto formatting

--- src/lib_appcommon/timebomb.cpp
+++ src/lib_appcommon/timebomb.cpp
@@ -5,51 +5,51 @@
 #include <sstream>
 
 namespace {
-	inline time_t time_when_compiled() {
-		// from: http://stackoverflow.com/questions/1765014/convert-string-from-date-into-a-time-t
-		std::string datestr = __DATE__;
-		std::string timestr = __TIME__;
-		std::istringstream iss_date(datestr);
-		std::string str_month;
-		int day;
-		int year;
+inline time_t time_when_compiled() {
+	// from: http://stackoverflow.com/questions/1765014/convert-string-from-date-into-a-time-t
+	std::string datestr = __DATE__;
+	std::string timestr = __TIME__;
+	std::istringstream iss_date(datestr);
+	std::string str_month;
+	int day;
+	int year;
 
-		iss_date >> str_month >> day >> year;
+	iss_date >> str_month >> day >> year;
 
-		int month = 0;
-		if (str_month == "Jan") month = 1;
-		else if (str_month == "Feb") month = 2;
-		else if (str_month == "Mar") month = 3;
-		else if (str_month == "Apr") month = 4;
-		else if (str_month == "May") month = 5;
-		else if (str_month == "Jun") month = 6;
-		else if (str_month == "Jul") month = 7;
-		else if (str_month == "Aug") month = 8;
-		else if (str_month == "Sep") month = 9;
-		else if (str_month == "Oct") month = 10;
-		else if (str_month == "Nov") month = 11;
-		else if (str_month == "Dec") month = 12;
-		else throw "Wrong month. Abort.";
+	int month = 0;
+	if (str_month == "Jan") month = 1;
+	else if (str_month == "Feb") month = 2;
+	else if (str_month == "Mar") month = 3;
+	else if (str_month == "Apr") month = 4;
+	else if (str_month == "May") month = 5;
+	else if (str_month == "Jun") month = 6;
+	else if (str_month == "Jul") month = 7;
+	else if (str_month == "Aug") month = 8;
+	else if (str_month == "Sep") month = 9;
+	else if (str_month == "Oct") month = 10;
+	else if (str_month == "Nov") month = 11;
+	else if (str_month == "Dec") month = 12;
+	else throw "Wrong month. Abort.";
 
-		for (std::string::size_type pos = timestr.find(':'); pos != std::string::npos; pos = timestr.find(':', pos)) {
-			timestr[pos] = ' ';
-		}
-
-		std::istringstream iss_time(timestr);
-		int hour, min, sec;
-		iss_time >> hour >> min >> sec;
-
-		tm t;
-		memset(&t, 0, sizeof(t));
-		t.tm_mon = month - 1;
-		t.tm_mday = day;
-		t.tm_year = year - 1900;
-		t.tm_hour = hour;
-		t.tm_min = min;
-		t.tm_sec = sec;
-
-		return mktime(&t);
+	for (std::string::size_type pos = timestr.find(':'); pos != std::string::npos; pos = timestr.find(':', pos)) {
+		timestr[pos] = ' ';
 	}
+
+	std::istringstream iss_time(timestr);
+	int hour, min, sec;
+	iss_time >> hour >> min >> sec;
+
+	tm t;
+	memset(&t, 0, sizeof(t));
+	t.tm_mon = month - 1;
+	t.tm_mday = day;
+	t.tm_year = year - 1900;
+	t.tm_hour = hour;
+	t.tm_min = min;
+	t.tm_sec = sec;
+
+	return mktime(&t);
+}
 }
 
 bool checkTimebomb(const double evaluation_period_in_days) {
