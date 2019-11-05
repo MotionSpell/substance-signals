6994d8982583334ce18165fb9018fa3f526d1883
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-11-05 22:11:35 +0100

    add missing file from previous commit

--- src/lib_media/in/mpeg_dash_input.hpp
+++ src/lib_media/in/mpeg_dash_input.hpp
@@ -4,16 +4,33 @@
 #include "lib_modules/utils/helper.hpp"
 #include "lib_media/common/file_puller.hpp"
 
+struct IAdaptationControl {
+	virtual int getNumAdaptationSets() const = 0;
+	virtual int getNumRepresentationsInAdaptationSet(int adaptationSetIdx) const = 0;
+
+	// no more that one stream per adaptation set can be enabled
+	// be aware that these calls will block until the command is effectively enqueued
+	virtual void enableStream(int asIdx, int repIdx) = 0;
+	virtual void disableStream(int asIdx) = 0;
+};
+
 struct DashMpd;
 
 namespace Modules {
 namespace In {
 
-class MPEG_DASH_Input : public Module {
+class MPEG_DASH_Input : public Module, public IAdaptationControl {
 	public:
 		MPEG_DASH_Input(KHost* host, IFilePullerFactory *filePullerFactory, std::string const &url);
 		~MPEG_DASH_Input();
 		void process() override;
+
+		// get information to perform bandwidth adaptation
+		// disabling all streams will stop the session
+		int getNumAdaptationSets() const override;
+		int getNumRepresentationsInAdaptationSet(int adaptationSetIdx) const override;
+		void enableStream(int asIdx, int repIdx) override;
+		void disableStream(int asIdx) override;
 
 	private:
 		KHost* const m_host;
