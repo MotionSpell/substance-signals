8c36afd52ed4fbf40be8ddfac58627ed61f1881c
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-01-05 12:22:46 +0100

    zen-cppredis: use lazy_git_clone

--- extra/pkg/zen-cppredis.sh
+++ extra/pkg/zen-cppredis.sh
@@ -1,20 +1,22 @@
 function cppredis_build {
-    git clone -b 4.3.1 --recursive  https://github.com/Cylix/cpp_redis.git cppredis
+  host=$1
 
+  readonly hash=bbe38a7f83de943ffcc90271092d689ae02b3489 #v4.3.1
+  lazy_git_clone https://github.com/Cylix/cpp_redis.git cppredis "$hash"
 
-    rm -rf cppredis/bin/$host
-    mkdir -p cppredis/bin/$host
-    pushDir cppredis/bin/$host
-    cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER="$host-gcc" \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_CXX_FLAGS=-I$PREFIX/include \
-        -DCMAKE_LD_FLAGS=-L$PREFIX/lib \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DCMAKE_INSTALL_LIBDIR=lib \
-        ../..
-    $MAKE
-    $MAKE install
-    popDir
+  rm -rf cppredis/bin/$host
+  mkdir -p cppredis/bin/$host
+  pushDir cppredis/bin/$host
+  cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER="$host-gcc" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS=-I$PREFIX/include \
+    -DCMAKE_LD_FLAGS=-L$PREFIX/lib \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    ../..
+  $MAKE
+  $MAKE install
+  popDir
 }
 
 function cppredis_get_deps {
