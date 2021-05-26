ad17423d8e8f358161231aad4faa4eea97acad49
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-05-26 13:54:40 +0200

    add missing file

---
+++ src/lib_utils/syslog.cpp
@@ -0,0 +1,45 @@
+#include "log.hpp"
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include <string>
+#include <stdexcept>
+
+#ifdef _WIN32
+void setGlobalLogSyslog(const char */*ident*/, const std::string &/*channel_name*/) {
+	throw std::runtime_error("Syslog is not supported on this platform");
+}
+#else
+
+#include <syslog.h>
+
+struct SyslogLogger : LogSink {
+	SyslogLogger(const char *ident, const std::string &channel_name) : channel_name(channel_name) {
+		openlog(ident, LOG_PID, LOG_USER|LOG_SYSLOG);
+	}
+	~SyslogLogger() {
+		closelog();
+	}
+	void send(Level level, const char* msg) override {
+		static const int levelToSysLog[] = {LOG_ERR, LOG_WARNING, LOG_INFO, LOG_DEBUG};
+
+		rapidjson::StringBuffer s;
+		rapidjson::Writer<rapidjson::StringBuffer> writer(s);
+		writer.StartObject();
+		writer.Key("message");
+		writer.String(msg);
+		writer.Key("tabatha_channel_name");
+		writer.String(channel_name.c_str());
+		writer.EndObject();
+
+		::syslog(levelToSysLog[level], s.GetString());
+	}
+	const std::string channel_name;
+};
+
+void setGlobalLogSyslog(const char *ident, const std::string &channel_name) {
+	static SyslogLogger syslogLogger(ident, channel_name);
+	g_Log = &syslogLogger;
+}
+
+#endif
