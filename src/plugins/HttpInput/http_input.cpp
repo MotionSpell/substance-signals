5f3cf42d7df370d42b7e7932b8074356720d28d9
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-12-12 20:59:26 +0100

    http in: restart after download completes (likely an error)

--- src/plugins/HttpInput/http_input.cpp
+++ src/plugins/HttpInput/http_input.cpp
@@ -1,6 +1,8 @@
 #include "http_input.hpp"
 #include "lib_modules/utils/factory.hpp" // registerModule
 #include "lib_modules/utils/helper.hpp"
+#include "lib_utils/format.hpp"
+#include "lib_utils/log_sink.hpp"
 #include "lib_utils/tools.hpp" // enforce
 #include "lib_media/common/file_puller.hpp"
 
@@ -11,24 +13,27 @@ std::unique_ptr<In::IFilePuller> createHttpSource();
 namespace {
 
 struct HttpInput : Module {
-		HttpInput(KHost *host, HttpInputConfig const& cfg) : url(cfg.url) {
+		HttpInput(KHost *host, HttpInputConfig const& cfg)
+			: m_host(host), url(cfg.url) {
 			addOutput();
 			host->activate(true);
 		}
 		void process() override {
-			if (!source) {
-				auto onBuffer = [&](SpanC chunk) {
-					auto data = std::make_shared<DataRaw>(chunk.len);
-					memcpy(data->buffer->data().ptr, chunk.ptr, chunk.len);
-					outputs[0]->post(data);
-				};
-
+			if (!source)
 				source = createHttpSource();
-				source->wget(url.c_str(), onBuffer);
-			}
+
+			auto onBuffer = [&](SpanC chunk) {
+				auto data = std::make_shared<DataRaw>(chunk.len);
+				memcpy(data->buffer->data().ptr, chunk.ptr, chunk.len);
+				outputs[0]->post(data);
+			};
+			m_host->log(Info, format("starting download of %s", url.c_str()).c_str());
+			source->wget(url.c_str(), onBuffer);
+			m_host->log(Info, format("download of %s completed", url.c_str()).c_str());
 		}
 
 	private:
+		KHost * const m_host;
 		std::string url;
 		std::unique_ptr<In::IFilePuller> source;
 };
