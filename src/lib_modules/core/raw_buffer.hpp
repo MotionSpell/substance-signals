c96b34a8c1e83b4690288b1d02bdc799ad6386ac
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2022-05-11 11:33:24 +0200

    revert change for growing-only raw buffers: we need a better allocator

--- src/lib_modules/core/raw_buffer.hpp
+++ src/lib_modules/core/raw_buffer.hpp
@@ -6,26 +6,23 @@
 namespace Modules {
 
 struct RawBuffer : IBuffer {
-		RawBuffer(size_t size) : memoryBlock(size), m_size(size) {}
+		RawBuffer(size_t size) : memoryBlock(size) {}
 
 		Span data() {
-			return Span { memoryBlock.data(), m_size };
+			return Span { memoryBlock.data(), memoryBlock.size() };
 		}
 
 		SpanC data() const {
-			return SpanC { memoryBlock.data(), m_size };
+			return SpanC { memoryBlock.data(), memoryBlock.size() };
 		}
 
 		// don't resize when shrinking
 		void resize(size_t size) {
-			m_size = size;
-			if (size > memoryBlock.size())
-				memoryBlock.resize(size);
+			memoryBlock.resize(size);
 		}
 
 	private:
 		std::vector<uint8_t> memoryBlock;
-		size_t m_size = 0;
 };
 
 }
